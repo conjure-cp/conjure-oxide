3f28dbbab01fbef1c33ec13c7d145669f63ec8d1
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-09-28 17:06:52 +0100

    minion: use minion's build system not build.rs for creating libminion.a

---
+++ solvers/minion/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+
+git submodule init
+git submodule update
+cd "$SCRIPT_DIR"
+
+echo "------ CONFIGURE STEP ------"
+
+if [ -d vendor/build ]; then
+  echo "vendor/build already exists; skipping"
+  echo "if you need to reconfigure minion (such as after an update), delete this directory!"
+else
+  mkdir -p vendor/build
+  cd vendor/build
+  python3 ../configure.py --library
+fi
+
+echo "------ BUILD STEP ------"
+cd "$SCRIPT_DIR"
+cd vendor/build
+make all
