7891f7e1ab70f761cb6eac2c1945af39f9dfda2a
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-10-10 13:31:04 +0100

    minion: change submodule url to minion/minion

--- solvers/minion/src/main.rs
+++ solvers/minion/src/main.rs
@@ -2,15 +2,10 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use minion_rs::bindings::MinionVersion;
 use std::ffi::c_char;
 use std::ffi::CString;
 
 pub fn main() {
-    unsafe {
-        let minionVersion: &str = std::str::from_utf8_unchecked(MinionVersion);
-        println!("{:?}", minionVersion);
-    }
 
     // https://stackoverflow.com/questions/34379641/how-do-i-convert-rust-args-into-the-argc-and-argv-c-equivalents
     let args = std::env::args()
