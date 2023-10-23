777e741dd80c9a0613d642e5ac2b03aff3d635b5
Özgür Akgün <ozgurakgun@gmail.com>
2023-10-23 21:34:44 +0100

    tree traversal examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 
-fn main() {
+// see all mainX functions, each one shows a different thing in action.
+
+// --------------------------------------------------------------------------------
+// constructing a model
+// and modifying the domain of a decision variable
+fn main1() {
     let a = Name::UserName(String::from("a"));
     let b = Name::UserName(String::from("b"));
     let c = Name::UserName(String::from("c"));
@@ -54,6 +59,9 @@ fn main() {
     println!("{:#?}", m);
 }
 
+// --------------------------------------------------------------------------------
+// the data structures
+
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 enum Name {
     UserName(String),
@@ -92,11 +100,103 @@ enum Range<A> {
     Bounded(A, A),
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Expression {
     ConstantInt(i32),
     Reference(Name),
     Sum(Vec<Expression>),
     Eq(Box<Expression>, Box<Expression>),
     Geq(Box<Expression>, Box<Expression>),
+}
+
+// --------------------------------------------------------------------------------
+// evaluating a sum of constants down to a single constant, not touching anything else.
+fn main2() {
+    let valid_sum_expression = Expression::Sum(vec![
+        Expression::ConstantInt(1),
+        Expression::ConstantInt(2),
+        Expression::ConstantInt(3),
+    ]);
+
+    let invalid_sum_expression = Expression::Sum(vec![
+        Expression::ConstantInt(1),
+        Expression::Reference(Name::UserName(String::from("a"))),
+    ]);
+
+    if let Some(result) = evaluate_sum_of_constants(&valid_sum_expression) {
+        println!("The sum is: {}", result); // Output: The sum is: 6
+    } else {
+        println!("The expression is not a sum of constant integers.");
+    }
+
+    if let Some(result) = evaluate_sum_of_constants(&invalid_sum_expression) {
+        println!("The sum is: {}", result);
+    } else {
+        println!("The expression is not a sum of constant integers."); // This will be printed
+    }
+}
+
+fn evaluate_sum_of_constants(expr: &Expression) -> Option<i32> {
+    match expr {
+        Expression::Sum(expressions) => {
+            let mut sum = 0;
+            for e in expressions {
+                match e {
+                    Expression::ConstantInt(value) => {
+                        sum += value;
+                    }
+                    _ => return None,
+                }
+            }
+            Some(sum)
+        }
+        _ => None,
+    }
+}
+
+// --------------------------------------------------------------------------------
+// applying evaluate_sum_of_constants recursively
+fn main3() {
+    let complex_expression = Expression::Eq(
+        Box::new(Expression::Sum(vec![
+            Expression::ConstantInt(1),
+            Expression::ConstantInt(2),
+            Expression::Sum(vec![Expression::ConstantInt(1), Expression::ConstantInt(2)]),
+            Expression::Reference(Name::UserName(String::from("a"))),
+        ])),
+        Box::new(Expression::ConstantInt(3)),
+    );
+
+    let simplified_expression = simplify_expression(complex_expression.clone());
+    println!("Original expression: {:#?}", complex_expression);
+    println!("Simplified expression: {:#?}", simplified_expression);
+}
+
+fn simplify_expression(expr: Expression) -> Expression {
+    match expr {
+        Expression::Sum(expressions) => {
+            if let Some(result) = evaluate_sum_of_constants(&Expression::Sum(expressions.clone())) {
+                Expression::ConstantInt(result)
+            } else {
+                Expression::Sum(expressions.into_iter().map(simplify_expression).collect())
+            }
+        }
+        Expression::Eq(left, right) => Expression::Eq(
+            Box::new(simplify_expression(*left)),
+            Box::new(simplify_expression(*right)),
+        ),
+        Expression::Geq(left, right) => Expression::Geq(
+            Box::new(simplify_expression(*left)),
+            Box::new(simplify_expression(*right)),
+        ),
+        _ => expr,
+    }
+}
+
+// --------------------------------------------------------------------------------
+
+fn main() {
+    // main1();
+    // main2();
+    main3();
 }
