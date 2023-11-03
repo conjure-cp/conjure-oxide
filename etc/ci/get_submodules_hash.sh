c54c6bba1b8e5d8228748c2eb9adef0d9f0b59c8
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-11-03 12:08:19 +0000

    CI: use single hash and rename things
    Qualify job names so they are easier to read in PRs.

--- etc/ci/get_submodules_hash.sh
+++ etc/ci/get_submodules_hash.sh
@@ -11,7 +11,7 @@ CI_SCRIPTS_DIR=$(realpath $(dirname "$0"))
 # go to top level of git repo
 cd $(git rev-parse --show-toplevel)
 
-git submodule update --init --recursive
+git submodule update --init --recursive 1>&2 2>/dev/null
 
 { 
   for module in $(sh "$CI_SCRIPTS_DIR/get_submodule_paths.sh")
@@ -19,4 +19,3 @@ git submodule update --init --recursive
     git rev-parse "HEAD:$module" >> $SHAS
   done;
 } 2>/dev/null | sha256sum | head -c 40 # note: sha256sum print a - at the end - this is removed here.
-echo ''
