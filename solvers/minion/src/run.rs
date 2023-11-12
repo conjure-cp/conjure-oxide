259e76a0e961246334c9f58611fce92e3d121467
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-11-12 13:23:08 +0000

    minion: add callback functionality

--- solvers/minion/src/run.rs
+++ solvers/minion/src/run.rs
@@ -1,4 +1,8 @@
-use std::ffi::CString;
+use std::{
+    collections::HashMap,
+    ffi::CString,
+    sync::{Mutex, MutexGuard},
+};
 
 use crate::{ast::*, error::*, raw_bindings::*, scoped_ptr::Scoped};
 
@@ -6,19 +10,63 @@ use crate::{ast::*, error::*, raw_bindings::*, scoped_ptr::Scoped};
 
 /// Callback function used to capture results from minion as they are generated.
 /// Should return `true` if search is to continue, `false` otherwise.
-pub type Callback = fn(Vec<(VarName, Constant)>) -> bool;
+pub type Callback = fn(HashMap<VarName, Constant>) -> bool;
+
+// Use globals to pass things between run_minion and the callback function.
+// Minion is (currently) single threaded anyways so the Mutexs' don't matter.
+
+// the current callback function
+static CALLBACK: Mutex<Option<Callback>> = Mutex::new(None);
+
+// the variables we want to return, and their ordering in the print matrix
+static PRINT_VARS: Mutex<Option<Vec<VarName>>> = Mutex::new(None);
 
 #[no_mangle]
-extern "C" fn hello_from_rust2() -> bool {
-    return true;
+unsafe extern "C" fn run_callback() -> bool {
+    // get printvars from static PRINT_VARS if they exist.
+    // if not, return true and continue search.
+
+    let mut guard: MutexGuard<'_, Option<Vec<VarName>>> = PRINT_VARS.lock().unwrap();
+
+    println!("here 0");
+    if guard.is_none() {
+        return true;
+    }
+
+    let print_vars = match &mut *guard {
+        Some(x) => x,
+        None => unreachable!(),
+    };
+
+    println!("here 1 printvars: {:?}", print_vars);
+    if print_vars.len() == 0 {
+        return true;
+    }
+
+    // build nice solutions view to be used by callback
+    let mut solutions: HashMap<VarName, Constant> = HashMap::new();
+
+    for (i, var) in print_vars.iter().enumerate() {
+        let solution_int: i32 = printMatrix_getValue(i as _).try_into().unwrap();
+        let solution: Constant = Constant::Integer(solution_int);
+        solutions.insert(var.to_string(), solution);
+    }
+
+    println!("here {:?}", solutions);
+    match *CALLBACK.lock().unwrap() {
+        None => true,
+        Some(func) => func(solutions),
+    }
 }
 
 pub fn run_minion(model: Model, callback: Callback) -> Result<(), RuntimeError> {
+    *CALLBACK.lock().unwrap() = Some(callback);
+
     unsafe {
         let options = Scoped::new(newSearchOptions(), |x| searchOptions_free(x as _));
         let args = Scoped::new(newSearchMethod(), |x| searchMethod_free(x as _));
         let instance = Scoped::new(convert_model_to_raw(&model), |x| instance_free(x as _));
-        let res = runMinion(options.ptr, args.ptr, instance.ptr, Some(hello_from_rust2));
+        let res = runMinion(options.ptr, args.ptr, instance.ptr, Some(run_callback));
 
         match res {
             0 => Ok(()),
@@ -47,9 +95,14 @@ unsafe fn convert_model_to_raw(model: &Model) -> *mut ProbSpec_CSPInstance {
 
     let search_vars = Scoped::new(vec_var_new(), |x| vec_var_free(x as _));
 
+    // store variables and the order they will be returned inside rust for later use.
+    let mut print_vars_guard = PRINT_VARS.lock().unwrap();
+    *print_vars_guard = Some(vec![]);
+
     for var_name in model.named_variables.get_variable_order() {
         //TODO: make this return Result
         let c_str = CString::new(var_name.clone()).expect("");
+
         let vartype = model
             .named_variables
             .get_vartype(var_name.clone())
@@ -71,6 +124,11 @@ unsafe fn convert_model_to_raw(model: &Model) -> *mut ProbSpec_CSPInstance {
         let var = getVarByName(instance, c_str.as_ptr() as _);
 
         printMatrix_addVar(instance, var);
+
+        // add to the print vars stored in rust so to remember
+        // the order for callback function.
+        (*print_vars_guard).as_mut().unwrap().push(var_name.clone());
+
         vec_var_push_back(search_vars.ptr, var);
     }
 
@@ -194,17 +252,34 @@ unsafe fn read_const(raw_constraint: *mut ProbSpec_ConstraintBlob, constant: &Co
 
 #[cfg(test)]
 mod tests {
-    use std::error::Error;
 
     use super::*;
 
-    /// .
-    fn callback(_: Vec<(VarName, Constant)>) -> bool {
-        return true;
-    }
-
     #[test]
     fn basic_ast_test() -> Result<(), RuntimeError> {
+        fn callback(solutions: HashMap<VarName, Constant>) -> bool {
+            let x = match solutions.get("x").unwrap() {
+                Constant::Integer(n) => n,
+                _ => panic!("x should be a integer"),
+            };
+
+            let y = match solutions.get("y").unwrap() {
+                Constant::Integer(n) => n,
+                _ => panic!("y should be a integer"),
+            };
+
+            let z = match solutions.get("z").unwrap() {
+                Constant::Integer(n) => n,
+                _ => panic!("z should be a integer"),
+            };
+
+            assert_eq!(*x, 1);
+            assert_eq!(*y, 2);
+            assert_eq!(*z, 1);
+
+            return true;
+        }
+
         let mut model = Model::new();
         model
             .named_variables
