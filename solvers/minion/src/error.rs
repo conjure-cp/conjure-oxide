e675efaf2d1018a5527866271d73c115786ae4ba
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-11-13 17:50:58 +0000

    Minion: Add callbacks for solutions fetching (#55)
    * CI: generate documentation
    
    * Bump the all group with 1 update
    
    Bumps the all group with 1 update: [cc](https://github.com/rust-lang/cc-rs).
    
    - [Release notes](https://github.com/rust-lang/cc-rs/releases)
    - [Commits](https://github.com/rust-lang/cc-rs/compare/1.0.83...1.0.84)
    
    ---
    updated-dependencies:
    - dependency-name: cc
      dependency-type: direct:production
      update-type: version-update:semver-patch
      dependency-group: all
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update Ci to new conjure_oxide paths
    
    * Update essence-feature-stats.yml
    
    * minion: update README and add licence
    
    * minion: add basic error handling to run_minion()
    
    * minion: add callback functionality
    
    * minion: update vendor
    
    * minion: convert run.rs test to a documentation example
    
    * minion: more docs and callback example
    
    * minion: docs - clarification on SEARCH and VARORDER
    
    * CI: do not generate doc coverage for dependencies
    
    * minion: generic error type
    
    * minion: remove debug prints
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Özgür Akgün <ozgurakgun@gmail.com>

---
+++ solvers/minion/src/error.rs
@@ -0,0 +1,45 @@
+//! Error types for Minion bindings.
+
+use crate::raw_bindings::*;
+use thiserror::Error;
+
+/// A wrapper over all errors thrown by `minion_rs`.
+///
+/// `Error` allows functions involving Minion to return a single error type. All error types in
+/// `minion_rs` are able to be converted into this type using into / from.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("runtime error: `{0}.to_string()`")]
+    RuntimeError(#[from] RuntimeError),
+}
+
+/// RuntimeErrors are thrown by Minion during execution.
+///
+/// These represent internal Minion C++ exceptions translated into Rust.
+///
+/// Invalid usage of this library should throw an error before Minion is even run. Therefore, these
+/// should be quite rare. Consider creating an issue on
+/// [Github](https://github.com/conjure-cp/conjure-oxide) if these occur regularly!
+#[derive(Debug, Error)]
+pub enum RuntimeError {
+    // These closely follow the ReturnCodes found in Minion's libwrapper.cpp.
+    /// The model given to Minion is invalid.
+    #[error("the given instance is invalid")]
+    InvalidInstance,
+
+    /// An unknown error has occurred.
+    #[error("an unknown error has occurred while running minion")]
+    UnknownError,
+}
+
+// Minion's ReturnCodes are passed over FFI as ints.
+// Convert them to their appropriate error.
+impl From<u32> for RuntimeError {
+    fn from(return_code: u32) -> Self {
+        match return_code {
+            #[allow(non_upper_case_globals)]
+            ReturnCodes_INVALID_INSTANCE => RuntimeError::InvalidInstance,
+            _ => RuntimeError::UnknownError,
+        }
+    }
+}
