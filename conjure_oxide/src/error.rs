4354d92517aa6f9a047893bdfefec6d1a0bdd8b6
Felix <felix@felixleitner.com>
2023-11-14 12:43:15 +0000

    Update error and clean parsing code

--- conjure_oxide/src/error.rs
+++ conjure_oxide/src/error.rs
@@ -3,16 +3,8 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("serde_json error: {0}")]
-    Json(#[from] JsonError),
-    #[error("Parse error: {0}")]
-    ParseError(String),
-    #[error("Error: {0}")]
-    Generic(String),
-}
-
-impl From<&str> for Error {
-    fn from(e: &str) -> Self {
-        Error::Generic(e.to_owned())
-    }
+    #[error("JSON parsing error: {0}")]
+    JsonError(#[from] JsonError),
+    #[error("Error constructing model: {0}")]
+    ModelConstructError(String),
 }
--- conjure_oxide/src/parse.rs
+++ conjure_oxide/src/parse.rs
@@ -2,20 +2,24 @@ use crate::ast::Model;
 use crate::error::Error;
 use serde_json::Value as JsonValue;
 
-pub fn parse_json(str: &String) -> Result<Model, Error> {
-    let v: JsonValue = serde_json::from_str(str)?;
-    let constraints: &Vec<JsonValue> = match &v["mStatements"] {
-        JsonValue::Array(a) => Ok(a),
-        _ => Err(Error::ParseError("mStatements is not an array".to_owned())),
-    }?;
+use Error::ModelConstructError as CError;
 
+pub fn parse_json(str: &String) -> Result<Model, Error> {
     let mut m = Model::new();
-    for c in constraints {
-        let obj = match c {
-            JsonValue::Object(obj) => Ok(obj),
-            _ => Err("Invalid JSON"),
-        }?;
-        println!("{:?}", c);
+    let v: JsonValue = serde_json::from_str(str)?;
+    let constraints = v["mStatements"]
+        .as_array()
+        .ok_or(CError("mStatements is not an array".to_owned()))?;
+
+    for con in constraints {
+        let obj = con
+            .as_object()
+            .ok_or(CError("mStatements contains a non-object".to_owned()))?;
+        let entry = obj
+            .iter()
+            .next()
+            .ok_or(CError("mStatements contains an empty object".to_owned()))?;
+        println!("{:?}", entry);
     }
 
     Ok(m)
