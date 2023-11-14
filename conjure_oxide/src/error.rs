4a571d13850f1e9987cfb3639652169f9fa90f22
Felix <felix@felixleitner.com>
2023-11-14 11:27:39 +0000

    Use thiserror to create From<> impl

--- conjure_oxide/src/error.rs
+++ conjure_oxide/src/error.rs
@@ -4,17 +4,11 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("serde_json error: {0}")]
-    Json(JsonError),
+    Json(#[from] JsonError),
     #[error("Parse error: {0}")]
     ParseError(String),
     #[error("Error: {0}")]
     Generic(String),
-}
-
-impl From<JsonError> for Error {
-    fn from(e: JsonError) -> Self {
-        Error::Json(e)
-    }
 }
 
 impl From<&str> for Error {
