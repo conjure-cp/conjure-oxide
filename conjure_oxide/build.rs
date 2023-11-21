f785e6fc392aa135eb4a4154d4db1a29321dd506
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-21 19:48:26 +0000

    tester rewrite
    - recursively walk all directories under tests/integration
    - generate a *.generated.* file
    - ACCEPT=true cargo test will accept all generated files as correct
    - add test outputs for the new test cases

--- conjure_oxide/build.rs
+++ conjure_oxide/build.rs
@@ -1,20 +1,20 @@
-use std::fs::read_dir;
-use std::io::Write;
-use std::{env::var, fs::File, path::Path};
+use std::env::var;
+use std::fs::{read_dir, File};
+use std::io::{self, Write};
+use std::path::Path;
 use walkdir::WalkDir;
 
-fn main() {
-    let out_dir = var("OUT_DIR").unwrap();
+fn main() -> io::Result<()> {
+    let out_dir = var("OUT_DIR").map_err(|e| io::Error::new(io::ErrorKind::Other, e))?; // wrapping in a std::io::Error to match main's error type
     let dest = Path::new(&out_dir).join("gen_tests.rs");
-    let mut f = File::create(&dest).unwrap();
+    let mut f = File::create(&dest)?;
 
     let test_dir = "tests/integration";
 
     for subdir in WalkDir::new(test_dir) {
-        let subdir = subdir.unwrap();
+        let subdir = subdir?;
         if subdir.file_type().is_dir() {
-            let essence_file_count = read_dir(subdir.path())
-                .expect("Failed to read directory")
+            let essence_files: Vec<String> = read_dir(subdir.path())?
                 .filter_map(Result::ok)
                 .filter(|entry| {
                     entry
@@ -22,21 +22,34 @@ fn main() {
                         .extension()
                         .map_or(false, |ext| ext == "essence")
                 })
-                .count();
-            if essence_file_count == 1 {
-                write_test(&mut f, subdir.path().display().to_string());
-            }
+                .filter_map(|entry| {
+                    entry
+                        .path()
+                        .file_stem()
+                        .and_then(|stem| stem.to_str())
+                        .map(|s| s.to_owned())
+                })
+                .collect();
+
+            write_test(&mut f, subdir.path().display().to_string(), essence_files)?;
         }
     }
+
+    Ok(())
 }
 
-fn write_test(file: &mut File, path: String) {
-    write!(
-        file,
-        include_str!("./tests/gen_test_template"),
-        // TODO: better sanitisation of paths to function names
-        name = path.replace("./", "").replace("/", "_").replace("-", "_"),
-        path = path
-    )
-    .unwrap();
+fn write_test(file: &mut File, path: String, essence_files: Vec<String>) -> io::Result<()> {
+    // TODO: Consider supporting multiple Essence files?
+    if essence_files.len() == 1 {
+        write!(
+            file,
+            include_str!("./tests/gen_test_template"),
+            // TODO: better sanitisation of paths to function names
+            test_name = path.replace("./", "").replace("/", "_").replace("-", "_"),
+            test_dir = path,
+            essence_file = essence_files[0]
+        )
+    } else {
+        Ok(())
+    }
 }
