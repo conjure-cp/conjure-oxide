d682d07e3e6b00033527bc6b96d98faf98f49b40
Felix <felix@felixleitner.com>
2023-11-22 11:17:18 +0000

    Use anyhow for better error handling

--- conjure_oxide/src/find_conjure.rs
+++ conjure_oxide/src/find_conjure.rs
@@ -1,46 +1,45 @@
 use versions::Versioning;
+use anyhow::{anyhow, Result, bail};
 
-pub fn conjure_executable() -> Result<String, String> {
-    let path = std::env::var("PATH")
-        .map_err(|_| "Could not read PATH environment variable".to_string())?;
+const CONJURE_MIN_VERSION: &str = "2.5.1";
+
+pub fn conjure_executable() -> Result<String> {
+    let path = std::env::var("PATH")?;
     let mut paths = std::env::split_paths(&path);
     let conjure_dir = paths
         .find(|path| path.join("conjure").exists())
-        .ok_or("Could not find conjure in PATH")?;
+        .ok_or(anyhow!("Could not find conjure in PATH"))?;
     let conjure_exec = conjure_dir.join("conjure")
         .to_str()
-        .ok_or("Could not unwrap conjure executable path")?
+        .ok_or(anyhow!("Could not unwrap conjure executable path"))?
         .to_string();
 
     let mut cmd = std::process::Command::new(&conjure_exec);
     let output = cmd
         .arg("--version")
-        .output()
-        .map_err(|_| "Could not execute conjure")?;
-    let stdout = String::from_utf8(output.stdout)
-        .map_err(|_| "Could not read conjure's stdout")?;
-    let stderr = String::from_utf8(output.stderr)
-        .map_err(|_| "Could not read conjure's stderr")?;
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+    let stderr = String::from_utf8(output.stderr)?;
 
     if !stderr.is_empty() {
-        return Err("Stderr is not empty: ".to_string() + &stderr);
+        bail!("Stderr is not empty: ".to_string() + &stderr);
     }
     let first = stdout
         .lines()
         .next()
-        .ok_or("Could not read conjure's stdout")?;
+        .ok_or(anyhow!("Could not read conjure's stdout"))?;
     if first != "Conjure: The Automated Constraint Modelling Tool" {
-        return Err("'conjure' points to an incorrect executable".to_string());
+        bail!("'conjure' points to an incorrect executable");
     }
     let second = stdout
         .lines()
         .nth(1)
-        .ok_or("Could not read conjure's stdout")?;
+        .ok_or(anyhow!("Could not read conjure's stdout"))?;
     let version = second
         .strip_prefix("Release version ")
-        .ok_or("Could not read conjure's stdout")?;
-    if Versioning::new(version) < Versioning::new("2.5.1") {
-        return Err("Conjure version is too old".to_string());
+        .ok_or(anyhow!("Could not read conjure's stdout"))?;
+    if Versioning::new(version) < Versioning::new(CONJURE_MIN_VERSION) {
+        bail!("Conjure version is too old (<{}): {}", CONJURE_MIN_VERSION, version);
     }
 
     Ok(conjure_exec)
