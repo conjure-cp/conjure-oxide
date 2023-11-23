9db3a5907461ef42859ecce31da7aaf4dc1cdc7c
Niklas Dewally <niklas@dewally.com>
2023-11-23 15:03:15 +0000

    minion_rs: always run ./configure.py when rebuilding

--- solvers/minion/build.sh
+++ solvers/minion/build.sh
@@ -10,14 +10,9 @@ cd "$SCRIPT_DIR"
 
 echo "------ CONFIGURE STEP ------"
 
-if [ -d vendor/build ]; then
-  echo "vendor/build already exists; skipping"
-  echo "if you need to reconfigure minion (such as after an update), delete this directory!"
-else
-  mkdir -p vendor/build
-  cd vendor/build
-  python3 ../configure.py --lib --quick
-fi
+mkdir -p vendor/build
+cd vendor/build
+python3 ../configure.py --lib --quick
 
 echo "------ BUILD STEP ------"
 cd "$SCRIPT_DIR"
