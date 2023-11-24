dd7d10ca7899ecab37942aab30c8cbeea64a09fa
Niklas Dewally <niklas@dewally.com>
2023-11-24 10:53:00 +0000

    refactor scripts

--- tools/update-submodules.sh
+++ tools/update-submodules.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 # vim: cc=80
-cd $(dirname "$0")
+
+# root of project
+cd $(dirname "$0")/..
 
 # update, initialise, and sync all submodules to ensure that changes are 
 # reflected here. `git submodule sync` means change where the submodule points 
 # to match .gitmodules, should it have changed (in particular, from a dev fork
 # to upstream).
 
-echo "=== Updating and resetting submodules ===" 1>&2
+echo "=== Cleaning submodules ===" 1>&2
 
-git submodule sync --recursive
-git submodule update --init --recursive
-
-echo "=== Cleaning submodule build files ===" 1>&2
-
-bash ./etc/scripts/get_submodule_paths.sh | 
+bash ./tools/plumbing/get_submodule_paths.sh | 
   {
   while read -r submodule_path; do
     cd "$submodule_path"
@@ -23,11 +20,9 @@ bash ./etc/scripts/get_submodule_paths.sh |
   done 
 }
 
-cd $(dirname "$0")
+echo "=== Updating and resetting submodules ===" 1>&2
 
-echo "=== Cargo clean ===" 1>&2
-echo "" 1>&2
-
-# clean our stuff
-cargo clean
+git submodule init --recursive
+git submodule sync --recursive
+git submodule update --init --recursive
 
