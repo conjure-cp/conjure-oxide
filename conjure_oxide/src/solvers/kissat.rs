d7242765dd215e1265c25f2aa69687b4c683cae5
gskorokhod <gs248@st-andrews.ac.uk>
2023-11-29 14:46:43 +0000

    remove .idea

--- conjure_oxide/src/solvers/kissat.rs
+++ conjure_oxide/src/solvers/kissat.rs
@@ -237,10 +237,9 @@ mod tests {
     use crate::ast::{DecisionVariable, Model};
     use crate::ast::{Expression, Name};
     use crate::solvers::kissat::{Error, CNF};
-    use std::collections::{HashMap, HashSet};
+    use std::collections::HashSet;
     use std::fmt::Debug;
     use std::hash::Hash;
-    use std::ops::Deref;
 
     fn to_set<T: Eq + Hash + Debug + Clone>(a: &Vec<T>) -> HashSet<T> {
         let mut a_set: HashSet<T> = HashSet::new();
