4a7ff28e4ecece50d78848e69657909cce133038
gskorokhod <gs248@st-andrews.ac.uk>
2024-01-26 19:59:46 +0000

    Add basic unwrapping for nested logical expressions

--- conjure_oxide/src/rules/mod.rs
+++ conjure_oxide/src/rules/mod.rs
@@ -79,3 +79,103 @@ fn lt_to_ineq(expr: &Expr) -> Result<Expr, RuleApplicationError> {
         _ => Err(RuleApplicationError::RuleNotApplicable),
     }
 }
+
+/**
+ * Unwrap nested `or`
+ * ```text
+ * or(or(a, b), c) = or(a, b, c)
+ * ```
+*/
+#[register_rule]
+fn unwrap_nested_or(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    match expr {
+        Expr::Or(exprs) => {
+            let mut new_exprs = Vec::new();
+            for e in exprs {
+                match e.as_ref() {
+                    Expr::Or(exprs) => {
+                        for e in exprs {
+                            new_exprs.push(e.clone());
+                        }
+                    }
+                    _ => new_exprs.push(e.clone()),
+                }
+            }
+            Ok(Expr::Or(new_exprs))
+        }
+        _ => Err(RuleApplicationError::RuleNotApplicable),
+    }
+}
+
+/**
+ * Unwrap nested `and`
+ * ```text
+ * and(and(a, b), c) = and(a, b, c)
+ * ```
+*/
+#[register_rule]
+fn unwrap_nested_and(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    match expr {
+        Expr::And(exprs) => {
+            let mut new_exprs = Vec::new();
+            for e in exprs {
+                match e.as_ref() {
+                    Expr::And(exprs) => {
+                        for e in exprs {
+                            new_exprs.push(e.clone());
+                        }
+                    }
+                    _ => new_exprs.push(e.clone()),
+                }
+            }
+            Ok(Expr::And(new_exprs))
+        }
+        _ => Err(RuleApplicationError::RuleNotApplicable),
+    }
+}
+
+/**
+ * Distribute `not` over `or`:
+ * ```text
+ * not (a or b) = (not a) and (not b)
+ * ```
+ */
+#[register_rule]
+fn distribute_not_or(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    match expr {
+        Expr::Not(expression) => match expression.as_ref() {
+            Expr::Or(exprs) => {
+                let mut new_exprs = Vec::new();
+                for e in exprs {
+                    new_exprs.push(Expr::Not(Box::new(e.clone())));
+                }
+                Ok(Expr::And(new_exprs))
+            }
+            _ => Err(RuleApplicationError::RuleNotApplicable),
+        },
+        _ => Err(RuleApplicationError::RuleNotApplicable),
+    }
+}
+
+/**
+ * Distribute `not` over `and`:
+ * ```text
+ * not (a and b) = (not a) or (not b)
+ * ```
+*/
+#[register_rule]
+fn distribute_not_and(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    match expr {
+        Expr::Not(expression) => match expression.as_ref() {
+            Expr::And(exprs) => {
+                let mut new_exprs = Vec::new();
+                for e in exprs {
+                    new_exprs.push(Expr::Not(Box::new(e.clone())));
+                }
+                Ok(Expr::Or(new_exprs))
+            }
+            _ => Err(RuleApplicationError::RuleNotApplicable),
+        },
+        _ => Err(RuleApplicationError::RuleNotApplicable),
+    }
+}
