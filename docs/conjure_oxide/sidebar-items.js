80f5ca1d210267d4ae39fab893faa3cda560891b
ozgurakgun <ozgurakgun@users.noreply.github.com>
2024-01-26 11:15:55 +0000

    Actions: Documentation for a0532d0f123e8418b6b6c2da54fb30c6596484e2

--- docs/conjure_oxide/sidebar-items.js
+++ docs/conjure_oxide/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Solver"],"mod":["ast","error","find_conjure","parse"],"struct":["Model"]};
+window.SIDEBAR_ITEMS = {"enum":["Solver"],"mod":["ast","error","find_conjure","parse","rules"],"struct":["Model"]};
