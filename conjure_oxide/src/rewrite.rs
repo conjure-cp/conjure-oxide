b2f35ba1012107cff19b005ccf8dbc25cd8ffdf8
Kieranoski <kfowlds1812@gmail.com>
2024-01-26 14:28:08 +0000

    Update rewrite.rs format

--- conjure_oxide/src/rewrite.rs
+++ conjure_oxide/src/rewrite.rs
@@ -16,33 +16,6 @@ pub fn rewrite(expression: &Expression) -> Expression {
     new
 }
 
-fn apply_all_rules<'a>(
-    expression: &'a Expression,
-    rules: &'a Vec<Rule<'a>>,
-) -> Vec<RuleResult<'a>> {
-    let mut results = Vec::new();
-    for rule in rules {
-        match rule.apply(expression) {
-            Ok(new) => {
-                results.push(RuleResult {
-                    rule: rule.clone(),
-                    new_expression: new,
-                });
-            }
-            Err(_) => continue,
-        }
-    }
-    results
-}
-
-fn choose_rewrite<'a>(results: &Vec<RuleResult<'a>>) -> Option<Expression> {
-    if results.is_empty() {
-        return None;
-    }
-    // Return the first result for now
-    Some(results[0].new_expression.clone())
-}
-
 /// # Returns
 /// - Some(<new_expression>) after applying the first applicable rule to `expr` or a sub-expression.
 /// - None if no rule is applicable to the expression or any sub-expression.
@@ -63,4 +36,31 @@ fn rewrite_iteration<'a>(
         }
     }
     None // No rules applicable to this branch of the expression
+}
+
+fn apply_all_rules<'a>(
+    expression: &'a Expression,
+    rules: &'a Vec<Rule<'a>>,
+) -> Vec<RuleResult<'a>> {
+    let mut results = Vec::new();
+    for rule in rules {
+        match rule.apply(expression) {
+            Ok(new) => {
+                results.push(RuleResult {
+                    rule: rule.clone(),
+                    new_expression: new,
+                });
+            }
+            Err(_) => continue,
+        }
+    }
+    results
+}
+
+fn choose_rewrite(results: &Vec<RuleResult>) -> Option<Expression> {
+    if results.is_empty() {
+        return None;
+    }
+    // Return the first result for now
+    Some(results[0].new_expression.clone())
 }
