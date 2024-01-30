7febe266ff80a8e0a7135ef3c9d06b2d07b15aef
ozgurakgun <ozgurakgun@users.noreply.github.com>
2024-01-30 09:34:35 +0000

    Actions: Documentation for 65ccb3c3d7a2cb7db991bb1c8f0d6270b40101ad

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,6 +1,6 @@
 var srcIndex = JSON.parse('{\
 "chuffed_rs":["",[],["lib.rs"]],\
-"conjure_oxide":["",[["rules",[],["mod.rs"]],["solvers",[],["error.rs","kissat.rs","minion.rs","mod.rs"]]],["error.rs","find_conjure.rs","lib.rs","parse.rs"]],\
+"conjure_oxide":["",[["rules",[],["mod.rs"]],["solvers",[],["error.rs","kissat.rs","minion.rs","mod.rs"]]],["error.rs","find_conjure.rs","lib.rs","parse.rs","rewrite.rs"]],\
 "minion_rs":["",[],["ast.rs","error.rs","ffi.rs","lib.rs","run.rs","scoped_ptr.rs"]]\
 }');
 createSrcSidebar();
