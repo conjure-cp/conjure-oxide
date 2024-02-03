3591bd4a389f4c380cd9cfd6a68f89a9e2630d49
Georgii Skorokhod <skorokhod.g@gmail.com>
2024-02-03 23:40:20 +0000

    Update Model struct to use new expressions

--- crates/conjure_core/src/ast.rs
+++ crates/conjure_core/src/ast.rs
@@ -16,7 +16,7 @@ impl Model {
     pub fn new() -> Model {
         Model {
             variables: HashMap::new(),
-            constraints: Expression::And(Vec::new()),
+            constraints: Expression::Nothing,
         }
     }
     // Function to update a DecisionVariable based on its Name
@@ -32,8 +32,19 @@ impl Model {
 
     pub fn get_constraints_vec(&self) -> Vec<Expression> {
         match &self.constraints {
-            Expression::And(exprs) => exprs.clone(),
-            _ => vec![],
+            Expression::And(constraints) => constraints.clone(),
+            Expression::Nothing => vec![],
+            _ => vec![self.constraints.clone()],
+        }
+    }
+
+    pub fn set_constraints(&mut self, constraints: Vec<Expression>) {
+        if (constraints.is_empty()) {
+            self.constraints = Expression::Nothing;
+        } else if (constraints.len() == 1) {
+            self.constraints = constraints[0].clone();
+        } else {
+            self.constraints = Expression::And(constraints);
         }
     }
 
@@ -42,13 +53,13 @@ impl Model {
         // We need to properly validate the expression but this is just for testing
         let mut constraints = self.get_constraints_vec();
         constraints.push(expression);
-        self.constraints = Expression::And(constraints);
+        self.set_constraints(constraints);
     }
 
     pub fn add_constraints(&mut self, expressions: Vec<Expression>) {
-        expressions
-            .iter()
-            .for_each(|e| self.add_constraint(e.clone()));
+        let mut constraints = self.get_constraints_vec();
+        constraints.extend(expressions);
+        self.set_constraints(constraints);
     }
 }
 
