7bd97b738320d9b00998b98046a67ec4ea19147e
Georgii Skorokhod <skorokhod.g@gmail.com>
2024-02-15 12:08:36 +0000

    Implement eq and hash for Rule

--- crates/conjure_core/src/lib.rs
+++ crates/conjure_core/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod ast;
 pub mod rule;
 
+mod rule_set;
 pub mod solvers;
+
 pub use solvers::Solver;
--- crates/conjure_core/src/rule.rs
+++ crates/conjure_core/src/rule.rs
@@ -1,4 +1,5 @@
 use std::fmt::{self, Display, Formatter};
+use std::hash::Hash;
 
 use thiserror::Error;
 
@@ -32,5 +33,19 @@ impl<'a> Rule<'a> {
 impl<'a> Display for Rule<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.name)
+    }
+}
+
+impl<'a> PartialEq for Rule<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl<'a> Eq for Rule<'a> {}
+
+impl<'a> Hash for Rule<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
     }
 }
