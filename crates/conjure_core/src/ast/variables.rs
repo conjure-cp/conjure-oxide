636f58d4ba4a5463b93b9cadc445e2968b07e8ad
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-10-05 10:20:26 +0100

    Added docstring to a DecisionVariable

--- crates/conjure_core/src/ast/variables.rs
+++ crates/conjure_core/src/ast/variables.rs
@@ -4,6 +4,28 @@ use serde::{Deserialize, Serialize};
 
 use crate::ast::domains::{Domain, Range};
 
+/// Represents a decision variable within a computational model.
+///
+/// A `DecisionVariable` has a domain that defines the set of values it can take. The domain could be:
+/// - A boolean domain, meaning the variable can only be `true` or `false`.
+/// - An integer domain, meaning the variable can only take specific integer values or a range of integers.
+///
+/// # Fields
+/// - `domain`:
+///   - Type: `Domain`
+///   - Represents the set of possible values that this decision variable can assume. The domain can be a range of integers
+///     (`IntDomain`) or a boolean domain (`BoolDomain`).
+///
+/// # Example
+/// ```
+/// use crate::ast::domains::{DecisionVariable, Domain, Range};
+///
+/// let bool_var = DecisionVariable::new(Domain::BoolDomain);
+/// let int_var = DecisionVariable::new(Domain::IntDomain(vec![Range::Bounded(1, 10)]));
+///
+/// println!("Boolean Variable: {}", bool_var);
+/// println!("Integer Variable: {}", int_var);
+/// ```
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct DecisionVariable {
     pub domain: Domain,
