471e5de5d2160e19c650bf8ae9bafee4154f1703
Niklas Dewally <niklas@dewally.com>
2024-11-08 14:02:26 +0000

    ast: impl Display for Factor

--- crates/conjure_core/src/ast/factor.rs
+++ crates/conjure_core/src/ast/factor.rs
@@ -15,3 +15,20 @@ pub enum Factor {
     Literal(Literal),
     Reference(Name),
 }
+
+impl std::fmt::Display for Factor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Factor::Literal(x) => x.fmt(f),
+            Factor::Reference(Name::UserName(n)) => {
+                write!(f, "{n}")?;
+                Ok(())
+            }
+
+            Factor::Reference(Name::MachineName(x)) => {
+                write!(f, "${x}")?;
+                Ok(())
+            }
+        }
+    }
+}
