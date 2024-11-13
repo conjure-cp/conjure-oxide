5194683a5c3c0a0355621123961e00657814362e
Devansh Chopra <devanshchopra@8afbe3d3.st-andrews.ac.uk>
2024-11-13 13:30:16 +0000

    Updated all sat_rs files by implementing a solver and mapping

---
+++ solvers/sat_rs/src/solver_utils.rs
@@ -0,0 +1,54 @@
+use rustsat_minisat::simp::Minisat;
+use crate::sat_solvers::SatSolver;
+use crate::sat_tree::conv_to_formula;
+use rustsat::instances::SatInstance;
+use anyhow::Result;
+
+pub fn initialize_solver(vec_problem: &Vec<Vec<i32>>) -> Result<(SatSolver<Minisat>, SatInstance)> {
+    let mut inst: SatInstance = SatInstance::new();
+    conv_to_formula(vec_problem, &mut inst)?;
+    let minisat_solver = Minisat::default();
+    let sat_solver = SatSolver::new(minisat_solver);
+    Ok((sat_solver, inst))
+}
+
+pub fn solve_problem(sat_solver: &SatSolver<Minisat>, instance: &SatInstance) -> Result<rustsat::solvers::SolverResult> {
+    let res = sat_solver.solve(instance)?;
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rustsat::instances::SatInstance;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_initialize_and_solve_satisfiable() {
+        let problem = vec![
+            vec![1, 2, -3],
+            vec![-1, 3],
+            vec![2, -3],
+            vec![-2, 3],
+            vec![1, -2],
+        ];
+
+        let (sat_solver, inst) = initialize_solver(&problem).unwrap();
+        let result = solve_problem(&sat_solver, &inst).unwrap();
+
+        assert_eq!(result, SolverResult::Sat);
+    }
+
+    #[test]
+    fn test_initialize_and_solve_unsatisfiable() {
+        let problem = vec![
+            vec![1],
+            vec![-1],
+        ];
+
+        let (sat_solver, inst) = initialize_solver(&problem).unwrap();
+        let result = solve_problem(&sat_solver, &inst).unwrap();
+
+        assert_eq!(result, SolverResult::Unsat);
+    }
+}
