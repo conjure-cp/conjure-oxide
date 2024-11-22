b57e6baf4e86108c06c3b856b65e2282778a9a2b
Felix <felix@felixleitner.com>
2024-11-22 21:30:06 +0000

    add rule selection via user input

--- crates/tree_morph/src/helpers.rs
+++ crates/tree_morph/src/helpers.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, io::Write};
+
 use crate::{Reduction, Rule};
 use multipeek::multipeek;
 use uniplate::Uniplate;
@@ -33,8 +35,68 @@ where
     rs.next().map(|(_, r)| r)
 }
 
+macro_rules! select_prompt {
+    () => {
+        "--- Current Expression ---
+{}
+
+--- Rules ---
+{}
+
+---
+q   No change
+<n> Apply rule n
+
+:"
+    };
+}
+
+pub fn select_user_input<T, M, R>(
+    t: &T,
+    rs: &mut dyn Iterator<Item = (&R, Reduction<T, M>)>,
+) -> Option<Reduction<T, M>>
+where
+    T: Uniplate + Display,
+    R: Rule<T, M> + Display,
+{
+    let mut choices: Vec<_> = rs.collect();
+
+    let rules = choices
+        .iter()
+        .enumerate()
+        .map(|(i, (r, Reduction { new_tree, .. }))| {
+            format!(
+                "{}. {}
+   ~> {}",
+                i + 1,
+                r,
+                new_tree
+            )
+        })
+        .collect::<Vec<_>>()
+        .join("\n\n");
+
+    loop {
+        print!(select_prompt!(), t, rules);
+        std::io::stdout().flush().unwrap(); // Print the : on same line
+
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+
+        match line.trim() {
+            "q" => return None,
+            n => {
+                if let Ok(n) = n.parse::<usize>() {
+                    if n > 0 && n <= choices.len() {
+                        let ret = choices.swap_remove(n - 1).1;
+                        return Some(ret);
+                    }
+                }
+            }
+        }
+    }
+}
+
 // TODO: Add more selection strategies:
 // - random
 // - smallest subtree
-// - ask the user for input via blocking I/O
-// - panic if there is more than 1 result, otherwise return the first
