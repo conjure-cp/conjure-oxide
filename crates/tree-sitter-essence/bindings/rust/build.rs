012282d3030fead30fb6c1d0697ff6c6a673094e
leiamcalisteryoung <l.mcalisteryoung@gmail.com>
2024-11-28 16:11:28 +0000

    updated build.rs to use tree-sitter-cli instead of globally downloaded tree-sitter

--- crates/tree-sitter-essence/bindings/rust/build.rs
+++ crates/tree-sitter-essence/bindings/rust/build.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
     let src_dir = std::path::Path::new("src");
@@ -9,10 +10,12 @@ fn main() {
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
 
-    let _output = Command::new("tree-sitter")
-        .args(["generate"])
-        .output()
-        .expect("Failed to execute tree-sitter generate");
+    let tree_sitter_cli = env::var("Cargo_BIN_EXE_tree-sitter").expect("tree-sitter-cli not found");
+    
+    std::process::Command::new(tree_sitter_cli)
+        .arg("generate")
+        .status()
+        .expect("Failed to run tree-sitter generate");
 
     let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
