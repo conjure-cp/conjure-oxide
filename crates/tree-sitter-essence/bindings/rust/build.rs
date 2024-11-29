8cc6010fcda7ea784ffb9a15279066d84f673ac1
leiamcalisteryoung <l.mcalisteryoung@gmail.com>
2024-11-29 00:40:09 +0000

    commmit so I can pull

--- crates/tree-sitter-essence/bindings/rust/build.rs
+++ crates/tree-sitter-essence/bindings/rust/build.rs
@@ -1,29 +1,17 @@
-// use std::process::Command;
-// use std::path::Path;
+use std::path::Path;
 use tree_sitter_generate::generate_parser_in_directory;
 
 fn main() {
-    let src_dir = std::path::Path::new("src");
+    let src_dir = Path::new("src");
 
-    generate_parser_in_directory(src_dir, Some("grammar.js"), 13, None, None).expect("Failed to generate parser");
+    generate_parser_in_directory(Path::new(""), Some("grammar.js"), 13, None, None)
+        .expect("Failed to generate parser");
 
     let mut c_config = cc::Build::new();
     c_config.std("c11").include(src_dir);
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
-
-    // Command::new("cargo")
-    //     .args(["run", "--package", "tree-sitter-cli", "--", "generate"])
-    //     .status()
-    //     .expect("Failed to execute tree-sitter generate");
-
-    // let tree_sitter_cli = env::var("CARGO_BIN_EXE_tree-sitter").expect("tree-sitter-cli not found");
-
-    // Command::new(tree_sitter_cli)
-    //     .arg("generate")
-    //     .status()
-    //     .expect("Failed to run tree-sitter generate");
 
     let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
