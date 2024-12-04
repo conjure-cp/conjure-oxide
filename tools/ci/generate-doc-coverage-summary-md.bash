dce769d957d67b79402bf8a8a1d784e66109fe41
Niklas Dewally <niklas@dewally.com>
2024-12-04 13:14:14 +0000

    tools: add script to print per crate doc coverage summaries

---
+++ tools/ci/generate-doc-coverage-summary-md.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# Prints a per-crate summary of documentation coverage in markdown, for consumption by Github Actions.
+#
+# Author: niklasdewally
+# Date: 2024/12/04
+
+CRATES="conjure_core conjure_oxide minion_rs tree-morph"
+JQ_SCRIPT=$(cat <<- 'EOF'
+# add percentages to each file
+
+[keys[] as $k
+  | .[$k]
+  | .file = $k
+  | .percentage = ((.with_docs / .total)*100 | round)
+  | .percentage_examples = ((.with_examples / .total)*100 | round)
+ ] as $coverage_info
+ | ($coverage_info | [.[].percentage] | add / length | round) as $avg
+ | ($coverage_info | [.[].percentage_examples] | add / length | round) as $avg_examples
+ | ($coverage_info | [.[].with_examples] | add) as $with_examples
+ | ($coverage_info | [.[].with_docs] | add) as $with_docs
+ | ($coverage_info | [.[].total] | add) as $total
+ | "\($avg)% documented, \($avg_examples)% with examples -- \($with_docs)/\($with_examples)/\($total)"
+EOF)
+
+for crate in ${CRATES}; do
+  echo -n "**${crate}:** "
+  RUSTDOCFLAGS='-Z unstable-options --show-coverage --output-format=json' cargo +nightly doc -p ${crate} --no-deps | jq -r "${JQ_SCRIPT}"
+done
