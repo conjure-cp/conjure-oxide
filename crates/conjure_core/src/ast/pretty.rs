a0f177c16826d0f598697fa858a16c164ed31272
Niklas Dewally <niklas@dewally.com>
2024-12-07 10:54:49 +0000

    fixup! feat(ast): add pretty printer for Vec<Expression>

--- crates/conjure_core/src/ast/pretty.rs
+++ crates/conjure_core/src/ast/pretty.rs
@@ -3,6 +3,8 @@
 //! Most things can be pretty printed using `Display`; however some, notably collections
 //! can not, for example, Vec<Expression>
 
+use std::fmt::Display;
+
 use itertools::Itertools;
 
 use super::Expression;
@@ -37,6 +39,23 @@ pub fn pretty_expressions_as_conjunction(expressions: &[Expression]) -> String {
 
     str.insert(0, '(');
     str.push(')');
+
+    str
+}
+
+/// Pretty prints a `Vec<T>` in a vector like syntax.
+///
+/// For some input values A,B,C:
+///
+/// ```text
+/// [A,B,C]
+/// ````
+///
+/// Each element is printed using its underlying `Display` implementation.
+pub fn pretty_vec<T: Display>(elems: &[T]) -> String {
+    let mut str = elems.iter().map(|x| format!("{}", x)).join(", ");
+    str.insert(0, '[');
+    str.push(']');
 
     str
 }
