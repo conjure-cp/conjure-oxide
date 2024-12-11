857ea90b23b51cb998e295d321df54e7a6e2e068
Devansh Chopra <devanshchopra@Devanshs-MacBook-Pro.local>
2024-12-11 17:59:10 +0000

    Pushing my semester work by adding Incremental Solving, Optimized Variable Mapping, Improved solve Method(will improve more), improved model handling by updating instantiate_model_from_conjure, created a new fucntion dynamic_clause_addition, added logs and eroor handling as well

--- crates/conjure_core/src/solver/adaptors/sat_adaptor.rs
+++ crates/conjure_core/src/solver/adaptors/sat_adaptor.rs
@@ -8,9 +8,9 @@ use clap::error;
 use minion_rs::ast::Model;
 use rustsat::encodings::am1::Def;
 use rustsat::solvers::{Solve, SolverResult};
-use rustsat::types::Var as satVar;
+use rustsat::types::{Var as satVar, Clause, Lit}; 
 use sat_rs::sat_tree::{self, conv_to_clause, conv_to_formula};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque}; 
 
 use rustsat_minisat::core::Minisat;
 
@@ -31,7 +31,6 @@ use super::super::SolverError::*;
 use super::super::SolverError::*;
 
 use rustsat::instances::SatInstance;
-
 use thiserror::Error;
 
 /// A [SolverAdaptor] for interacting with the SatSolver generic and the types thereof.
@@ -39,8 +38,10 @@ use thiserror::Error;
 pub struct SAT {
     __non_constructable: private::Internal,
     model_inst: Option<SatInstance>,
-    var_map: Option<HashMap<i32, satVar>>,
+    var_map: Option<HashMap<i32, satVar>>,         // Mapping ConjureModel -> Minisat variables
+    reverse_var_map: Option<HashMap<satVar, i32>>, // Reverse mapping Minisat -> ConjureModel variables
     solver_inst: Minisat,
+    incremental_clauses: VecDeque<Clause>,
 }
 
 impl private::Sealed for SAT {}
@@ -51,7 +52,9 @@ impl Default for SAT {
             __non_constructable: private::Internal,
             model_inst: None,
             var_map: None,
+            reverse_var_map: None,
             solver_inst: Minisat::default(),
+            incremental_clauses: VecDeque::new(),
         }
     }
 }
@@ -63,8 +66,42 @@ impl SAT {
             __non_constructable: private::Internal,
             model_inst: model_to_use,
             var_map: None,
+            reverse_var_map: None,
             solver_inst: Minisat::default(),
+            incremental_clauses: VecDeque::new(),
         }
+    }
+
+    // Adds a new clause to the SAT solver incrementally.
+    pub fn add_incremental_clause(&mut self, clause: Vec<i32>) -> Result<(), SolverError> {
+        let new_clause: Clause = clause
+            .iter()
+            .map(|&lit| {
+                let abs_lit = lit.abs() as u32;
+                if lit > 0 {
+                    Lit::positive(abs_lit)
+                } else {
+                    Lit::negative(abs_lit)
+                }
+            })
+            .collect();
+
+        // Add the clause to the deque for keeping records
+        self.incremental_clauses.push_back(new_clause.clone());
+        println!("Added incremental clause: {:?}", clause);
+        // Added the clause directly to Minisat
+        match self.solver_inst.add_clause(new_clause) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(SolverError::Runtime(format!("{:?}", e))),
+        }
+    }
+
+    pub fn get_sat_var(&self, model_var: i32) -> Option<satVar> {
+        self.var_map.as_ref()?.get(&model_var).copied()
+    }
+
+    pub fn get_model_var(&self, sat_var: satVar) -> Option<i32> {
+        self.reverse_var_map.as_ref()?.get(&sat_var).copied()
     }
 
     pub fn add_clause_to_mod(&self, clause_vec: Vec<i32>) -> () {}
@@ -72,72 +109,97 @@ impl SAT {
 
 pub fn instantiate_model_from_conjure(
     conjure_model: ConjureModel,
-) -> Result<SatInstance, SolverError> {
+) -> Result<(SatInstance, HashMap<i32, satVar>, HashMap<satVar, i32>), SolverError> {
     let mut inst: SatInstance = SatInstance::new();
+    let mut var_map: HashMap<i32, satVar> = HashMap::new();
+    let mut reverse_var_map: HashMap<satVar, i32> = HashMap::new();
 
-    for var_name_ref in conjure_model.variables.keys() {
-        let curr_decision_var = conjure_model
-            .variables
-            .get(var_name_ref)
-            .ok_or_else(|| ModelInvalid(format!("variable {:?} not found", var_name_ref)))?;
+    for (var_name_ref, decision_var) in conjure_model.variables.iter() {
+        let cdom = &decision_var.domain;
+        if cdom != &conjure_ast::Domain::BoolDomain {
+            return Err(ModelFeatureNotSupported(format!(
+                "variable {:?}: expected BoolDomain but found: {:?}",
+                decision_var, decision_var.domain
+            )));
+        }
 
-        {
-            // todo: the scope change may be unneeded
-            // check domain, err if bad domain
-            let cdom = &curr_decision_var.domain;
-            if cdom != &conjure_ast::Domain::BoolDomain {
-                return Err(ModelFeatureNotSupported(format!(
-                    "variable {:?}: expected BoolDomain, found: {:?}",
-                    curr_decision_var, curr_decision_var.domain
+        let sat_var = inst.new_var();
+        let var_id = match var_name_ref {
+            Name::MachineName(id) => *id, // Extracts integer ID from Name
+            _ => {
+                println!("Unsupported variable name format: {:?}", var_name_ref);
+                return Err(SolverError::Runtime(format!(
+                    "Unsupported variable name format: {:?}",
+                    var_name_ref
                 )));
             }
-        }
+        };
+
+        var_map.insert(var_id, sat_var);
+        reverse_var_map.insert(sat_var, var_id);
     }
-
-    let md = Metadata {
-        clean: false,
-        etype: None,
-    };
-
     let constraints_vec: Vec<Expression> = conjure_model.get_constraints_vec();
-    let vec_cnf = handle_and(Expression::And(md, constraints_vec));
+    let vec_cnf = handle_and(Expression::And(
+        Metadata {
+            clean: false,
+            etype: None,
+        },
+        constraints_vec,
+    ));
     conv_to_formula(&(vec_cnf.unwrap()), &mut inst);
 
-    Ok(inst)
+    Ok((inst, var_map, reverse_var_map))
 }
 
 impl SolverAdaptor for SAT {
+
+    fn load_model(&mut self, model: ConjureModel, _: private::Internal) -> Result<(), SolverError> {
+        let (inst, var_map, reverse_var_map) = instantiate_model_from_conjure(model)?;
+        self.model_inst = Some(inst);
+        self.var_map = Some(var_map);
+        self.reverse_var_map = Some(reverse_var_map);
+        Ok(())
+    }
+
     fn solve(
         &mut self,
         callback: SolverCallback,
         _: private::Internal,
     ) -> Result<SolveSuccess, SolverError> {
-        // solver = self.solver
-        // handle
-        let cnf_func: rustsat::instances::Cnf = self.model_inst.clone().unwrap().into_cnf().0;
-        &self.solver_inst.add_cnf(cnf_func);
-        let res = &self.solver_inst.solve().unwrap();
-
+        let cnf_func = match self.model_inst.clone() {
+            Some(model) => model.into_cnf().0,
+            None => return Err(SolverError::Runtime("No model instance found.".to_owned())),
+        };
+    
+        self.solver_inst.add_cnf(cnf_func).map_err(|e| {
+            SolverError::Runtime(format!("Failed to add CNF to the solver: {:?}", e))
+        })?;
+        let res = self.solver_inst.solve().map_err(|e| {
+            SolverError::Runtime(format!("Solver encountered an error: {:?}", e))
+        })?;
+    
         let solver_res = match res {
             SolverResult::Sat => true,
             SolverResult::Unsat => false,
-
-            // should not arise:
-            SolverResult::Interrupted => Err(SolverError::Runtime(format!("SatInstance may be invalid, Interrupted.")))?,
+            SolverResult::Interrupted => {
+                return Err(SolverError::Runtime(
+                    "SatInstance may be invalid: Interrupted.".to_owned(),
+                ));
+            }
         };
-
-        // error thrown always. impermanent
-        // will eventually have a SolveSucess instance being returned with Ok(), when the implementation is more permanent.
-        // Ok(SolveSuccess{
-        //     // stats tbd, fails
-        //     stats:SolverStats::with_timings(10.0),
-        //     status: SearchStatus::Complete(())
-        // });
-        print!("{}", solver_res);
-
+    
+        // Gives SAT results
+        if solver_res {
+            if let Ok(solution) = self.solver_inst.full_solution() {
+                println!("SAT Solution: {:?}", solution);
+            } else {
+                println!("Unable to extract full solution from the solver.");
+            }
+        }
+        println!("{}", solver_res);
         Err(OpNotImplemented("solve_mut".to_owned()))
-        // Ok(())
     }
+    
 
     fn solve_mut(
         &mut self,
@@ -145,12 +207,6 @@ impl SolverAdaptor for SAT {
         _: private::Internal,
     ) -> Result<SolveSuccess, SolverError> {
         Err(OpNotSupported("solve_mut".to_owned()))
-    }
-
-    fn load_model(&mut self, model: ConjureModel, _: private::Internal) -> Result<(), SolverError> {
-        let inst_res = instantiate_model_from_conjure(model);
-        self.model_inst = Some(inst_res.unwrap());
-        Ok(())
     }
 
     fn get_family(&self) -> SolverFamily {
@@ -201,10 +257,6 @@ pub fn handle_or(e: Expression) -> Result<(Vec<i32>), CNFError> {
         _ => Err(CNFError::UnexpectedExpression(e))?,
     };
 
-    // if vec_clause.len() != 2 {
-    //     panic!("Villain, What hast thou done?\nThat which thou canst not undo.")
-    // };
-
     let mut ret_clause: Vec<i32> = Vec::new();
 
     for expr in vec_clause {
@@ -214,7 +266,6 @@ pub fn handle_or(e: Expression) -> Result<(Vec<i32>), CNFError> {
             _ => Err(CNFError::UnexpectedExpressionInsideOr(expr))?,
         }
     }
-
     Ok(ret_clause)
 }
 
@@ -235,27 +286,35 @@ pub fn handle_and(e: Expression) -> Result<(Vec<Vec<i32>>), CNFError> {
 
     Ok(ret_vec_of_vecs)
 }
+
+fn dynamic_clause_addition(sat_solver: &mut SAT) {
+    let dynamic_clause = vec![1, -2, 3];
+    if let Err(e) = sat_solver.add_incremental_clause(dynamic_clause) {
+        println!("Error adding clause: {:?}", e);
+    }
+}
+
 //CNF Error, may be replaced of integrated with error file
 #[derive(Error, Debug)]
 pub enum CNFError {
-    #[error("Variable with name `{0}` not found")]
+    #[error("Variable with name {0} not found")]
     VariableNameNotFound(conjure_ast::Name),
 
-    #[error("Variable with name `{0}` not of right type")]
+    #[error("Variable with name {0} not of right type")]
     BadVariableType(Name),
 
-    #[error("Unexpected Expression `{0}` inside Not(). Only Not(Reference) or Not(Not) allowed!")]
+    #[error("Unexpected Expression {0} inside Not(). Only Not(Reference) or Not(Not) allowed!")]
     UnexpectedExpressionInsideNot(Expression),
 
-    #[error("Unexpected Expression `{0}` as literal. Only Not() or Reference() allowed!")]
+    #[error("Unexpected Expression {0} as literal. Only Not() or Reference() allowed!")]
     UnexpectedLiteralExpression(Expression),
 
-    #[error("Unexpected Expression `{0}` inside And(). Only And(vec<Or>) allowed!")]
+    #[error("Unexpected Expression {0} inside And(). Only And(vec<Or>) allowed!")]
     UnexpectedExpressionInsideAnd(Expression),
 
-    #[error("Unexpected Expression `{0}` inside Or(). Only Or(lit, lit) allowed!")]
+    #[error("Unexpected Expression {0} inside Or(). Only Or(lit, lit) allowed!")]
     UnexpectedExpressionInsideOr(Expression),
 
-    #[error("Unexpected Expression `{0}` found!")]
+    #[error("Unexpected Expression {0} found!")]
     UnexpectedExpression(Expression)
 }
