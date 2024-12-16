c2b2d32e6591bf59ebdd1fa145ca3b0f1350b807
Shikhar Srivastava <109688678+Shikhar-Srivastava-16@users.noreply.github.com>
2024-12-17 01:00:39 +0530

    rename + auxiliary file deleted

--- crates/conjure_core/src/solver/adaptors/mod.rs
+++ crates/conjure_core/src/solver/adaptors/mod.rs
@@ -4,10 +4,10 @@
 // pub use kissat::Kissat;
 #[doc(inline)]
 pub use minion::Minion;
-pub use sat_adaptor::SAT;
+pub use rustsat::SAT;
 // mod sat_common;
 
 // mod kissat;
 mod minion;
-pub mod sat_adaptor;                                //temp visibility
+pub mod rustsat;                                //temp visibility
 // mod common;
