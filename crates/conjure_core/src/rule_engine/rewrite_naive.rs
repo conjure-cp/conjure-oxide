b06c967191bd443297007d968181e6000c0d8277
Niklas Dewally <niklas@dewally.com>
2025-02-03 09:58:41 +0000

    refactor: simplify naive rewriter using `Biplate<Expr>`
    Simplify the naive rewriter logic by implementing `Biplate<Expression>`
    for `Model`.
    
    Currently, we have two similar functions to rewrite expressions in value
    lettings and the expression tree. By implementing `Biplate<Expression>`
    in this commit, the constraints tree and expressions inside the symbol
    table can be traversed using the same operation. This makes it possible to
    use a single rewriting loop for both value lettings and the constraints
    tree.
    
    Due to limitations in Uniplate derive (documented in code comments),
    this commit uses manual implementations for the Uniplate / Biplate
    traits for `Model` and `SymbolTable`; however, the features needed to
    make derive work here have been added to the Uniplate issue tracker, so
    we should be able to remove these in the future.

--- crates/conjure_core/src/rule_engine/rewrite_naive.rs
+++ crates/conjure_core/src/rule_engine/rewrite_naive.rs
@@ -1,6 +1,6 @@
 use super::{RewriteError, RuleSet};
 use crate::{
-    ast::{Expression as Expr, Name},
+    ast::Expression as Expr,
     bug,
     rule_engine::{
         get_rule_priorities,
@@ -10,10 +10,9 @@ use crate::{
     Model,
 };
 
-use crate::ast::pretty::pretty_value_letting_declaration;
 use std::collections::{BTreeMap, HashSet};
 use std::sync::Arc;
-use uniplate::{Biplate, Uniplate as _};
+use uniplate::Biplate;
 
 /// A naive, exhaustive rewriter for development purposes. Applies rules in priority order,
 /// favouring expressions found earlier during preorder traversal of the tree.
@@ -35,22 +34,12 @@ pub fn rewrite_naive<'a>(
     let mut model = model.clone();
 
     // Rewrite until there are no more rules left to apply.
-    //
-    // 1. try to rewrite value lettings.
-    // 2. try to rewrite the model (the main event).
 
-    while let Some(()) = try_rewrite_value_lettings(
+    while let Some(()) = try_rewrite_model(
         &mut model,
         &rules_by_priority,
         prop_multiple_equally_applicable,
-    )
-    .or_else(|| {
-        try_rewrite_model(
-            &mut model,
-            &rules_by_priority,
-            prop_multiple_equally_applicable,
-        )
-    }) {}
+    ) {}
 
     Ok(model)
 }
@@ -63,12 +52,14 @@ fn try_rewrite_model<'a>(
     rules_by_priority: &Vec<(u16, HashSet<&'a Rule<'a>>)>,
     prop_multiple_equally_applicable: bool,
 ) -> Option<()> {
-    type CtxFn = Arc<dyn Fn(Expr) -> Vec<Expr>>;
+    type CtxFn = Arc<dyn Fn(Expr) -> Model>;
     let mut results: Vec<(RuleResult<'_>, u16, Expr, CtxFn)> = vec![];
 
     // Iterate over rules by priority in descending order.
     'top: for (priority, rule_set) in rules_by_priority.iter().rev() {
-        for (expr, ctx) in <_ as Biplate<Expr>>::contexts_bi(&model.get_constraints_vec()) {
+        // Using Biplate, rewrite both the expression tree, and any value lettings in the symbol
+        // table.
+        for (expr, ctx) in <_ as Biplate<Expr>>::contexts_bi(model) {
             // Clone expr and ctx so they can be reused
             let expr = expr.clone();
             let ctx = ctx.clone();
@@ -119,116 +110,7 @@ fn try_rewrite_model<'a>(
             log_rule_application(result, expr, model);
 
             // Replace expr with new_expression
-            model.set_constraints(ctx(result.reduction.new_expression.clone()));
-
-            // Apply new symbols and top level
-            result.reduction.clone().apply(model);
-        }
-    }
-
-    Some(())
-}
-
-// Tries to do a single rewrite on the value lettings of the model.
-//
-// Returns None if no change was made.
-fn try_rewrite_value_lettings<'a>(
-    model: &mut Model,
-    rules_by_priority: &Vec<(u16, HashSet<&'a Rule<'a>>)>,
-    prop_multiple_equally_applicable: bool,
-) -> Option<()> {
-    // I don't like this clone, but otherwise I run into borrow checker issues with multiple
-    // borrows of model.
-    for (name, _) in model.symbols().clone().iter_value_letting() {
-        if try_rewrite_value_letting(
-            name,
-            model,
-            rules_by_priority,
-            prop_multiple_equally_applicable,
-        )
-        .is_some()
-        {
-            return Some(());
-        }
-    }
-
-    None
-}
-
-// Tries to do a single rewrite on the given value letting.
-//
-// Returns None if no change was made.
-fn try_rewrite_value_letting<'a>(
-    name: &Name,
-    model: &mut Model,
-    rules_by_priority: &Vec<(u16, HashSet<&'a Rule<'a>>)>,
-    prop_multiple_equally_applicable: bool,
-) -> Option<()> {
-    type CtxFn = Arc<dyn Fn(Expr) -> Expr>;
-    let mut results: Vec<(RuleResult<'_>, u16, Expr, CtxFn)> = vec![];
-
-    // Originally this was a &mut Expression.
-    //
-    // However, this counted as a mutable borrow of the model (as this expression is in the model), meaning
-    // we couldn't borrow the model again later on.
-    let root_expr = model.symbols().get_value_letting(name)?.clone();
-    'top: for (priority, rule_set) in rules_by_priority.iter().rev() {
-        for (expr, ctx) in root_expr.contexts() {
-            // Clone expr and ctx so they can be reused
-            let expr = expr.clone();
-            let ctx = ctx.clone();
-            for rule in rule_set {
-                match (rule.application)(&expr, model) {
-                    Ok(red) => {
-                        // Collect applicable rules
-                        results.push((
-                            RuleResult {
-                                rule,
-                                reduction: red,
-                            },
-                            *priority,
-                            expr.clone(),
-                            ctx.clone(),
-                        ));
-                    }
-                    #[allow(clippy::unwrap_used)]
-                    Err(_) => {
-                        // when called a lot, this becomes very expensive!
-                        #[cfg(debug_assertions)]
-                        tracing::trace!(
-                            "Rule attempted but not applied: {} (priority {}), to declaration {}",
-                            rule.name,
-                            priority,
-                            pretty_value_letting_declaration(model.symbols(), name).unwrap()
-                        );
-                    }
-                }
-            }
-            // This expression has the highest rule priority so far, so this is what we want to
-            // rewrite.
-            if !results.is_empty() {
-                break 'top;
-            }
-        }
-    }
-
-    match results.as_slice() {
-        [] => {
-            return None;
-        } // no rules are applicable.
-        [(result, _priority, expr, ctx), ..] => {
-            if prop_multiple_equally_applicable {
-                assert_no_multiple_equally_applicable_rules(&results, rules_by_priority);
-            }
-
-            // Extract the single applicable rule and apply it
-            log_rule_application(result, expr, model);
-
-            // Replace expr with new_expression
-            model.symbols_mut().update_add_value_letting(
-                name.clone(),
-                ctx(result.reduction.new_expression.clone()),
-            )?;
+            *model = ctx(result.reduction.new_expression.clone());
 
             // Apply new symbols and top level
             result.reduction.clone().apply(model);
