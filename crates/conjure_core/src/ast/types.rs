0e5453a94d733bc7ab2483e174a79038c8d6252f
Niklas Dewally <niklas@dewally.com>
2025-02-23 11:30:39 +0000

    matrix type

--- crates/conjure_core/src/ast/types.rs
+++ crates/conjure_core/src/ast/types.rs
@@ -1,10 +1,21 @@
 use serde::{Deserialize, Serialize};
 
+use super::Range;
+
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
 pub enum ReturnType {
     Int,
     Bool,
     Set,
+
+    /// A matrix or 1-dimensional slice.
+    Matrix {
+        /// The indices of each dimension of the matrix.
+        dimensions: Vec<Vec<Range<i32>>>,
+        
+        /// The type of values in the matrix.
+        values: Box<ReturnType>,
+    },
 }
 
 /// Something with a return type
