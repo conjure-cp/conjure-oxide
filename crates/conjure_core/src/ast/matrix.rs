2a2b749c62a3d10736ec690c47094bb5e4b962e7
Niklas Dewally <niklas@dewally.com>
2025-03-18 21:22:03 +0000

    feat: evaluation of indexing and slicing of matrix literals (#730)

--- crates/conjure_core/src/ast/matrix.rs
+++ crates/conjure_core/src/ast/matrix.rs
@@ -2,9 +2,12 @@
 
 // TODO: Georgiis essence macro would look really nice in these examples!
 
-use itertools::Itertools;
+use std::{collections::VecDeque, sync::Arc};
 
-use crate::ast::Domain;
+use itertools::{izip, Itertools};
+use uniplate::Uniplate as _;
+
+use crate::ast::{Domain, Range};
 
 use super::{AbstractLiteral, Literal};
 
@@ -59,6 +62,10 @@ pub fn flatten(matrix: AbstractLiteral<Literal>) -> impl Iterator<Item = Literal
         panic!("matrix should be a matrix");
     };
 
+    flatten_1(elems)
+}
+
+fn flatten_1(elems: Vec<Literal>) -> impl Iterator<Item = Literal> {
     elems.into_iter().flat_map(|elem| {
         if let Literal::AbstractLiteral(m @ AbstractLiteral::Matrix(_, _)) = elem {
             Box::new(flatten(m)) as Box<dyn Iterator<Item = Literal>>
@@ -66,4 +73,69 @@ pub fn flatten(matrix: AbstractLiteral<Literal>) -> impl Iterator<Item = Literal
             Box::new(std::iter::once(elem)) as Box<dyn Iterator<Item = Literal>>
         }
     })
+}
+/// Flattens a multi-dimensional matrix literal into an iterator over (indices,element).
+///
+/// # Panics
+///
+///   + If the number or type of elements in each dimension is inconsistent.
+///
+///   + If `matrix` is not a matrix.
+///
+///   + If any dimensions in the matrix are not finite or enumerable with [`Domain::values`].
+///     However, index domains in the form `int(i..)` are supported.
+pub fn flatten_enumerate(
+    matrix: AbstractLiteral<Literal>,
+) -> impl Iterator<Item = (Vec<Literal>, Literal)> {
+    let AbstractLiteral::Matrix(elems, _) = matrix.clone() else {
+        panic!("matrix should be a matrix");
+    };
+
+    let index_domains = index_domains(matrix)
+        .into_iter()
+        .map(|mut x| match x {
+            // give unboundedr index domains an end
+            Domain::IntDomain(ref mut ranges) if ranges.len() == 1 && !elems.is_empty() => {
+                if let Range::UnboundedR(start) = ranges[0] {
+                    ranges[0] = Range::Bounded(start, start + (elems.len() as i32 - 1));
+                };
+                x
+            }
+            _ => x,
+        })
+        .collect_vec();
+
+    izip!(enumerate_indices(index_domains), flatten_1(elems))
+}
+
+/// Gets the index domains for a matrix literal.
+///
+/// # Panics
+///
+/// + If `matrix` is not a matrix.
+///
+/// + If the number or type of elements in each dimension is inconsistent.
+pub fn index_domains(matrix: AbstractLiteral<Literal>) -> Vec<Domain> {
+    let AbstractLiteral::Matrix(_, _) = matrix else {
+        panic!("matrix should be a matrix");
+    };
+
+    matrix.cata(Arc::new(
+        move |element: AbstractLiteral<Literal>, child_index_domains: VecDeque<Vec<Domain>>| {
+            assert!(
+                child_index_domains.iter().all_equal(),
+                "each child of a matrix should have the same index domain"
+            );
+
+            let child_index_domains = child_index_domains.front().cloned().unwrap_or(vec![]);
+            match element {
+                AbstractLiteral::Set(_) => vec![],
+                AbstractLiteral::Matrix(_, domain) => {
+                    let mut index_domains = vec![domain];
+                    index_domains.extend(child_index_domains);
+                    index_domains
+                }
+            }
+        },
+    ))
 }
