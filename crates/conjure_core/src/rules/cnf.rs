809ac2f14330c8c2c7bebd651d02dc3b715f25af
Hecent <74259874+hecent@users.noreply.github.com>
2025-03-23 22:45:05 +0000

    Added  remove_implication rule with a basic test

--- crates/conjure_core/src/rules/cnf.rs
+++ crates/conjure_core/src/rules/cnf.rs
@@ -5,4 +5,36 @@
 use conjure_core::rule_engine::register_rule_set;
 use conjure_core::solver::SolverFamily;
 
+use conjure_core::ast::Expression as Expr;
+use conjure_core::metadata::Metadata;
+use conjure_core::rule_engine::{
+    register_rule, ApplicationError, ApplicationError::RuleNotApplicable, ApplicationResult,
+    Reduction,
+};
+
+use Expr::*;
+
+use crate::ast::{Atom, SymbolTable};
+use crate::{into_matrix_expr, matrix_expr};
+
 register_rule_set!("CNF", ("Base"), (SolverFamily::SAT));
+
+/// Converts an implication to cnf
+///
+/// ```text
+/// x -> y ~> !x \/ y
+/// ```
+#[register_rule(("CNF", 4100))]
+fn remove_implication(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
+    let Expr::Imply(_, x, y) = expr else {
+        return Err(RuleNotApplicable);
+    };
+
+    Ok(Reduction::pure(Expr::Or(
+        Metadata::new(),
+        Box::new(matrix_expr![
+            Expr::Not(Metadata::new(), x.clone()),
+            *y.clone()
+        ]),
+    )))
+}
