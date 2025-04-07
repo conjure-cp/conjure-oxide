9fdfcaca818c04e41dcecf8dd72c91c7ca93ff1f
Niklas Dewally <niklas@dewally.com>
2025-04-07 16:35:03 +0100

    feat: move guards rule for and
    Add rule to convert dynamic guards inside an and comprehension to static
    guards if possible.
    
    For some return expression inside an and comprehension containing a
    dynamic guard, g -> e, move g into the static guards if it only
    references induction variables.
    
    Static guards are used to prune the possible assignments for induction
    variables during matrix expansion, meaning that this reformulation
    results in smaller models and better performance.

--- crates/conjure_rules/src/expand_comprehension.rs
+++ crates/conjure_rules/src/expand_comprehension.rs
@@ -1,6 +1,7 @@
 use conjure_core::{
     ast::{Expression as Expr, SymbolTable},
     into_matrix_expr,
+    metadata::Metadata,
     rule_engine::{
         register_rule, ApplicationError::RuleNotApplicable, ApplicationResult, Reduction,
     },
@@ -22,4 +23,29 @@ fn expand_comprehension(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult
         .or(Err(RuleNotApplicable))?;
 
     Ok(Reduction::with_symbols(into_matrix_expr!(results), symbols))
+}
+
+#[register_rule(("Base", 8000))]
+fn comprehension_move_static_guard_into_generators(
+    expr: &Expr,
+    _: &SymbolTable,
+) -> ApplicationResult {
+    let Expr::Comprehension(_, comprehension) = expr else {
+        return Err(RuleNotApplicable);
+    };
+
+    let mut comprehension = comprehension.clone();
+    let Expr::Imply(_, guard, expr) = comprehension.clone().return_expression() else {
+        return Err(RuleNotApplicable);
+    };
+
+    if comprehension.add_induction_guard(*guard) {
+        comprehension.replace_return_expression(*expr);
+        Ok(Reduction::pure(Expr::Comprehension(
+            Metadata::new(),
+            comprehension,
+        )))
+    } else {
+        Err(RuleNotApplicable)
+    }
 }
