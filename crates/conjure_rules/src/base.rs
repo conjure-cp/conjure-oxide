3f76b699d02774772d5c19d4d3d60752679d5f75
Georgii Skorokhod <skorokhod.g@gmail.com>
2025-04-13 14:24:07 +0100

    fix import

--- crates/conjure_rules/src/base.rs
+++ crates/conjure_rules/src/base.rs
@@ -8,6 +8,7 @@ use conjure_core::{
         register_rule, register_rule_set, ApplicationError, ApplicationResult, Reduction,
     },
 };
+use conjure_essence_macros::essence_expr;
 use uniplate::Uniplate;
 
 use ApplicationError::RuleNotApplicable;
