6f778ef34f374cdc3af7ac1105c685afd1a877a7
Tudor Huica <th201@st-andrews.ac.uk>
2025-04-14 18:31:30 +0100

    added another case where we try to rewrite the size

--- crates/conjure_core/src/rules/sets/cardinality.rs
+++ crates/conjure_core/src/rules/sets/cardinality.rs
@@ -10,12 +10,10 @@ use Expression::*;
 
 use crate::ast::{Declaration, SetAttr};
 use crate::rule_engine::Reduction;
-// TODO: comment explanations
 
-// TODO: add cardinality depending on existing constraints if needed??
-// TODO: check special cases like : 0 < |A| < 2 => |A| = 1 and other exceptions if needed??
-// TODO: should we cover the case : |A| != a (Neq) ??
-#[register_rule(("Base", 8900))]
+// rule that loads the cardinality of a set into the attribute of a set
+// this rule is only applicable if the set is a decision variable and the cardinality is not already loaded
+#[register_rule(("Base", 8901))]
 fn card_to_attr(expr: &Expression, st: &SymbolTable) -> ApplicationResult {
     let mut code = 0;
     let (a, b) = match expr {
@@ -39,64 +37,116 @@ fn card_to_attr(expr: &Expression, st: &SymbolTable) -> ApplicationResult {
             code = 5;
             (Some(a), Some(b))
         }
-        // Neq(_, a, b) => {
-        //     code = 6;
-        //     (Some(a), Some(b))
-        // }
         _ => (None, None),
     };
-    if let Some(a) = a {
-        if let Some(b) = b {
-            if let (Abs(_, e), Atomic(_, d)) = (a.as_ref(), b.as_ref()) {
-                if let (Atomic(_, Atom::Reference(g)), Atom::Literal(Literal::Int(size))) =
-                    (e.as_ref(), d)
-                {
-                    if let Some(r) = st.lookup(g) {
-                        if let DeclarationKind::DecisionVariable(var) = r.kind() {
-                            if let Domain::DomainSet(atr, vals) = &var.domain {
-                                let mut symbols = st.clone();
-                                match atr {
-                                    SetAttr::None => {
-                                        let new_domain = match code {
-                                            1 => Domain::DomainSet(
-                                                SetAttr::Size(*size),
-                                                vals.clone(),
-                                            ),
-                                            2 => Domain::DomainSet(
-                                                SetAttr::MaxSize(*size),
-                                                vals.clone(),
-                                            ),
-                                            3 => Domain::DomainSet(
-                                                SetAttr::MinSize(*size),
-                                                vals.clone(),
-                                            ),
-                                            4 => Domain::DomainSet(
-                                                SetAttr::MaxSize(*size - 1),
-                                                vals.clone(),
-                                            ),
-                                            5 => Domain::DomainSet(
-                                                SetAttr::MinSize(*size + 1),
-                                                vals.clone(),
-                                            ),
-                                            _ => Domain::DomainSet(SetAttr::None, vals.clone()),
-                                        };
+    if let (Some(a), Some(b)) = (a, b) {
+        if let (Abs(_, e), Atomic(_, d)) = (a.as_ref(), b.as_ref()) {
+            // d can't be a variable since the cardinality will be treated differently depending on the representation of the set
+            if let (Atomic(_, Atom::Reference(g)), Atom::Literal(Literal::Int(size))) =
+                (e.as_ref(), d)
+            {
+                if let Some(r) = st.lookup(g) {
+                    if let DeclarationKind::DecisionVariable(var) = r.kind() {
+                        if let Domain::DomainSet(atr, vals) = &var.domain {
+                            let mut symbols = st.clone();
+                            let mut new_domain = Domain::DomainSet(atr.clone(), vals.clone());
+                            match atr {
+                                SetAttr::Size(s) => {
+                                    match code {
+                                        1 => {
+                                            if s == size {
+                                                return Err(RuleNotApplicable);
+                                            } else {
+                                                return Ok(Reduction::pure(Expression::Atomic(
+                                                    Metadata::new(),
+                                                    Atom::Literal(Literal::Bool(false)),
+                                                )));
+                                            }
+                                        }
 
-                                        symbols.update_insert(Rc::new(Declaration::new_var(
-                                            g.clone(),
-                                            new_domain,
-                                        )));
-                                        println!("{:?}", symbols.lookup(g));
-                                        return Ok(Reduction::with_symbols(
-                                            Expression::Atomic(
-                                                Metadata::new(),
-                                                Atom::Literal(Literal::Bool(true)),
-                                            ),
-                                            symbols,
-                                        ));
-                                    }
-                                    _ => todo!(),
+                                        2 => {
+                                            if s <= size {
+                                                return Err(RuleNotApplicable);
+                                            } else {
+                                                return Ok(Reduction::pure(Expression::Atomic(
+                                                    Metadata::new(),
+                                                    Atom::Literal(Literal::Bool(false)),
+                                                )));
+                                            }
+                                        }
+                                        3 => {
+                                            if s >= size {
+                                                return Err(RuleNotApplicable);
+                                            } else {
+                                                return Ok(Reduction::pure(Expression::Atomic(
+                                                    Metadata::new(),
+                                                    Atom::Literal(Literal::Bool(false)),
+                                                )));
+                                            }
+                                        }
+                                        4 => {
+                                            if s < size {
+                                                return Err(RuleNotApplicable);
+                                            } else {
+                                                return Ok(Reduction::pure(Expression::Atomic(
+                                                    Metadata::new(),
+                                                    Atom::Literal(Literal::Bool(false)),
+                                                )));
+                                            }
+                                        }
+                                        5 => {
+                                            if s > size {
+                                                return Err(RuleNotApplicable);
+                                            } else {
+                                                return Ok(Reduction::pure(Expression::Atomic(
+                                                    Metadata::new(),
+                                                    Atom::Literal(Literal::Bool(false)),
+                                                )));
+                                            }
+                                        }
+                                        _ => return Err(RuleNotApplicable),
+                                    };
+                                }
+                                SetAttr::None => {
+                                    new_domain = match code {
+                                        1 => Domain::DomainSet(SetAttr::Size(*size), vals.clone()),
+                                        2 => {
+                                            Domain::DomainSet(SetAttr::MaxSize(*size), vals.clone())
+                                        }
+                                        3 => {
+                                            Domain::DomainSet(SetAttr::MinSize(*size), vals.clone())
+                                        }
+                                        4 => Domain::DomainSet(
+                                            SetAttr::MaxSize(*size - 1),
+                                            vals.clone(),
+                                        ),
+                                        5 => Domain::DomainSet(
+                                            SetAttr::MinSize(*size + 1),
+                                            vals.clone(),
+                                        ),
+                                        _ => {
+                                            println!("Case not covered!");
+                                            return Err(RuleNotApplicable);
+                                        }
+                                    };
+                                }
+                                _ => {
+                                    println!("Case not covered!");
+                                    Domain::DomainSet(SetAttr::None, vals.clone());
                                 }
                             }
+                            symbols.update_insert(Rc::new(Declaration::new_var(
+                                g.clone(),
+                                new_domain,
+                            )));
+                            println!("{:?}", symbols.lookup(g));
+                            return Ok(Reduction::with_symbols(
+                                Expression::Atomic(
+                                    Metadata::new(),
+                                    Atom::Literal(Literal::Bool(true)),
+                                ),
+                                symbols,
+                            ));
                         }
                     }
                 }
