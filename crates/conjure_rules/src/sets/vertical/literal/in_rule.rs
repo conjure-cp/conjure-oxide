1d19cc47fd5fc95741dd6feff8bd1e17748e80d0
Tudor Huica <th201@st-andrews.ac.uk>
2025-05-04 13:05:00 +0100

    used try_from and fixed the extraction of i32 from literals

--- crates/conjure_rules/src/sets/vertical/literal/in_rule.rs
+++ crates/conjure_rules/src/sets/vertical/literal/in_rule.rs
@@ -20,51 +20,24 @@ fn in_set(expr: &Expression, _: &SymbolTable) -> ApplicationResult {
     match expr {
         In(_, a, b) => {
             let mut literals = Vec::new();
-            let mut retur = true;
 
-            // TODO: add case where a is a variable
-            match b.as_ref() {
-                AbstractLiteral(_, c) => match c {
-                    AbstractLiteral::Set(t) => {
-                        for expr in t {
-                            if let Atomic(_, Atom::Literal(Literal::Int(i))) = expr {
-                                literals.push(*i);
-                            } else {
-                                retur = false;
-                                break;
-                            }
-                        }
-                    }
-                    _ => retur = false,
-                },
-                Atomic(_, Atom::Literal(Literal::AbstractLiteral(AbstractLiteral::Set(c)))) => {
-                    for number in c {
-                        if let Literal::Int(n) = number {
-                            literals.push(*n);
-                        } else {
-                            retur = false;
-                        }
-                    }
+            if let Atomic(_, Atom::Literal(Literal::AbstractLiteral(AbstractLiteral::Set(c)))) =
+                b.as_ref()
+            {
+                for number in c {
+                    literals.push(i32::try_from(number.clone()).unwrap());
                 }
-
-                _ => return Err(RuleNotApplicable),
             }
-            if retur {
-                if literals.is_empty() {
-                    return Ok(Reduction::pure(Atomic(
-                        Metadata::new(),
-                        Atom::Literal(Literal::Bool(false)),
-                    )));
-                }
-                if let Atomic(_, a) = a.as_ref() {
-                    Ok(Reduction::pure(Expression::MinionWInSet(
-                        Metadata::new(),
-                        a.clone(),
-                        literals,
-                    )))
-                } else {
-                    Err(RuleNotApplicable)
-                }
+
+            if literals.is_empty() {
+                return Ok(Reduction::pure(false.into()));
+            }
+            if let Atomic(_, a) = a.as_ref() {
+                Ok(Reduction::pure(Expression::MinionWInSet(
+                    Metadata::new(),
+                    a.clone(),
+                    literals,
+                )))
             } else {
                 Err(RuleNotApplicable)
             }
