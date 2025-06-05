ca7562544537a78cf2900a6223d78cafb85aca13
Niklas Dewally <niklas@dewally.com>
2025-06-05 09:41:45 +0100

    WIP better comprehension expanding for and and or

--- crates/conjure_rules/src/expand_comprehension.rs
+++ crates/conjure_rules/src/expand_comprehension.rs
@@ -1,11 +1,56 @@
 use conjure_core::{
-    ast::{Expression as Expr, SymbolTable},
+    ast::{Expression as Expr, Literal, ReturnType, SymbolTable, Typeable as _},
     into_matrix_expr,
     metadata::Metadata,
     rule_engine::{
         register_rule, ApplicationError::RuleNotApplicable, ApplicationResult, Reduction,
     },
 };
+
+use uniplate::Uniplate;
+
+use conjure_rule_macros::register_rule_set;
+
+
+// optimised comprehension expansion for associative-commutative operators
+register_rule_set!("Better_AC_Comprehension_Expansion", ("Base"));
+
+/// expand compatible comprehensions using ac optimisations / Comprehension::expand_ac.
+
+
+#[register_rule(("Better_AC_Comprehension_Expansion", 2001))]
+fn expand_comprehension_ac(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult {
+    // Is this an ac expression?
+    let ac_operator_kind = expr.to_ac_operator_kind().ok_or(RuleNotApplicable)?;
+
+    // FIXME: currently only support and/or, but should eventually support all ac operators
+    // see Comprehension::expand_ac.
+
+    let Literal::Bool(_) = ac_operator_kind.identity() else {
+        return Err(RuleNotApplicable);
+    };
+
+    // ac operators should only have one child
+    debug_assert_eq!(
+        expr.children().len(),
+        1,
+        "AC expressions should have a exactly one child."
+    );
+
+    let Expr::Comprehension(_, ref comprehension) = expr.children()[0] else {
+        return Err(RuleNotApplicable);
+    };
+
+    // TODO: check what kind of error this throws and maybe panic
+    let mut symbols = symbols.clone();
+    let results = comprehension
+        .clone()
+        .expand_ac(&mut symbols, ac_operator_kind)
+        .or(Err(RuleNotApplicable))?;
+
+    let new_expr = ac_operator_kind.to_expression(into_matrix_expr!(results));
+    Ok(Reduction::with_symbols(new_expr, symbols))
+}
 
 #[register_rule(("Base", 2000))]
 fn expand_comprehension(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult {
@@ -19,7 +64,7 @@ fn expand_comprehension(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult
     let results = comprehension
         .as_ref()
         .clone()
-        .solve_with_minion(&mut symbols)
+        .expand_simple(&mut symbols)
         .or(Err(RuleNotApplicable))?;
 
     Ok(Reduction::with_symbols(into_matrix_expr!(results), symbols))
