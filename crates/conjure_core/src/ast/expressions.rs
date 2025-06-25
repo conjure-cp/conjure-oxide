conjure-cp/conjure-oxide

commit 2ae7a8f3cd941fe77579d24b5632f7e4ec20f778

feat(ast): add assertion for size of Expression type

crates/conjure_core/src/ast/expressions.rs
@@ -24,6 +24,23 @@ use super::comprehension::Comprehension;
 use super::records::RecordValue;
 use super::{Domain, Range, SubModel, Typeable};
 
+// Ensure that this type doesn't get too big
+//
+// If you triggered this assertion, you either made a variant of this enum that is too big, or you
+// made Name,Literal,AbstractLiteral,Atom bigger, which made this bigger! To fix this, put some
+// stuff in boxes.
+//
+// Enums take the size of their largest variant, so an enum with mostly small variants and a few
+// large ones wastes memory... A larger Expression type also slows down Oxide.
+//
+// For more information, and more details on type sizes and how to measure them, see the commit
+// message for 6012de809 (perf: reduce size of AST types, 2025-06-18).
+//
+// https://github.com/conjure-cp/conjure-oxide/commit/6012de8096ca491ded91ecec61352fdf4e994f2e
+
+// expect size of Expression to be 96 bytes
+static_assertions::assert_eq_size!([u8; 96], Expression);
+
 /// Represents different types of expressions used to define rules and constraints in the model.
 ///
 /// The `Expression` enum includes operations, constants, and variable references

crates/conjure_core/Cargo.toml
@@ -37,6 +37,7 @@ rustsat-minisat = "0.7.2"
 rustsat = "0.7.2"
 tree-morph = { path = "../tree_morph" }
 paste = "1.0.15"
+static_assertions = "1.1.0"
 
 [dev-dependencies]
 conjure_essence_macros = { path = "../conjure_essence_macros" }

commit 6581c38d3db610d88633bf40d047ff264732b852

doc: show type sizes in API docs
Show type sizes in the rustdoc API documentation.

This may be useful to debug failing type size assertions as introduced
in  2ae7a8f3cd (feat(ast): add assertion for size of Expression type, 2025-06-19).

crates/conjure_core/src/ast/expressions.rs
@@ -36,6 +36,8 @@ use super::{Domain, Range, SubModel, Typeable};
 // For more information, and more details on type sizes and how to measure them, see the commit
 // message for 6012de809 (perf: reduce size of AST types, 2025-06-18).
 //
+// You can also see type sizes in the rustdoc documentation, generated by ./tools/gen_docs.sh
+//
 // https://github.com/conjure-cp/conjure-oxide/commit/6012de8096ca491ded91ecec61352fdf4e994f2e
 
 // expect size of Expression to be 96 bytes

tools/gen_docs.sh
@@ -26,4 +26,4 @@ TARGET_DIR=$(cargo metadata 2> /dev/null | jq -r .target_directory 2>/dev/null)
 cd "$PROJECT_ROOT"
 rm -rf "$TARGET_DIR/doc"
 
-RUSTDOCFLAGS="-Zunstable-options --markdown-no-toc" cargo +nightly doc --no-deps --all-features -p conjure_oxide -p conjure_rule_macros -p conjure_core -p minion_rs -p enum_compatability_macro -p conjure_essence_parser -p conjure_essence_macros $@
+RUSTDOCFLAGS="-Zunstable-options --show-type-layout --markdown-no-toc" cargo +nightly doc --no-deps --all-features -p conjure_oxide -p conjure_rule_macros -p conjure_core -p minion_rs -p enum_compatability_macro -p conjure_essence_parser -p conjure_essence_macros $@