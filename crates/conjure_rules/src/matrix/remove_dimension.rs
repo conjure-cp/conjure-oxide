f29e0cc725d1eaf63ba5d9107436e97ae685a475
Niklas Dewally <niklas@dewally.com>
2025-07-14 19:58:32 +0100

    feat(rules/minion): add rules to handle matrix literals
    Add rules to handle expressions involving matrix literals, including
    indexing them with a variable (`[1,2,3,4][i]`), and flattening inside
    them (e.g. `[1,2,e/4,4*f] ~~> [1,2,__0,__1]`). Matrix literals inside
    and,or, sum,product expressions are excluded as these expressions
    already have their own flattening methods, or don't need flattening
    atall.
    
    This also includes the start of 2d matrix indexing with the
    `remove_dimension_from_matrix_index` rule. We currently only support
    indexing 2d matrix literals (e.g. `[[1,2,3],[4,5,6]][i,j]`), but it
    should be simple enough to extend this to 2d matrix indexing in general.

---
+++ crates/conjure_rules/src/matrix/remove_dimension.rs
@@ -0,0 +1,41 @@
+use conjure_core::ast::{Expression as Expr, SymbolTable};
+use conjure_core::into_matrix_expr;
+use conjure_core::metadata::Metadata;
+use conjure_core::rule_engine::{
+    ApplicationError::RuleNotApplicable, ApplicationResult, Reduction, register_rule,
+};
+
+/// Removes a dimension from a matrix indexing operation, if the subject of the indexing is a
+/// matrix literal.
+///
+/// ```plain
+/// [[1,2,3],[4,5,6],[7,8,9]][j,i] ~~> [[1,2,3][i],[4,5,6][i],[7,8,9][i]][j]
+/// ```
+#[register_rule(("Base", 2000))]
+fn remove_dimension_from_matrix_indexing(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
+    let Expr::SafeIndex(_, subject, mut indices) = expr.clone() else {
+        return Err(RuleNotApplicable);
+    };
+
+    if indices.len() < 2 {
+        return Err(RuleNotApplicable);
+    };
+
+    // the indicies to use in the replacement expression.
+    let outer_indices = vec![indices.pop().unwrap()];
+
+    // the indicies to use in the inner expressions.
+    let inner_indices = indices;
+
+    let (mut es, index_domain) = subject.unwrap_matrix_unchecked().ok_or(RuleNotApplicable)?;
+
+    for e in es.iter_mut() {
+        *e = Expr::SafeIndex(Metadata::new(), Box::new(e.clone()), inner_indices.clone());
+    }
+
+    Ok(Reduction::pure(Expr::SafeIndex(
+        Metadata::new(),
+        Box::new(into_matrix_expr![es;index_domain]),
+        outer_indices,
+    )))
+}
