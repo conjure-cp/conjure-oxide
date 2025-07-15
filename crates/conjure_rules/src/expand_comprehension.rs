582533f9d063e8f977f15e76f7303d8bb509cb16
Niklas Dewally <niklas@dewally.com>
2025-07-15 14:56:27 +0100

    feat: better comprehension expansion
    Add better comprehension expansion for ac operators ("expand-ac"). This
    performs pruning on a comprehension by passing the generator model to
    Minion.
    
    This is enabled by the `Better_Comprehension_Expansion` ruleset. This
    ruleset is on by default,  but can be turned off with the
    `--no-use-expand-ac` CLI flag.
    
    For more details, please read our (upcoming) modref paper "Solver-aided
    expansion of loops"

--- crates/conjure_rules/src/expand_comprehension.rs
+++ crates/conjure_rules/src/expand_comprehension.rs
@@ -1,15 +1,68 @@
+use std::collections::VecDeque;
+
 use conjure_core::{
-    ast::{Expression as Expr, SymbolTable},
+    ast::{Expression as Expr, SymbolTable, comprehension::Comprehension},
     into_matrix_expr,
-    metadata::Metadata,
     rule_engine::{
         ApplicationError::RuleNotApplicable, ApplicationResult, Reduction, register_rule,
     },
 };
+use uniplate::Biplate;
+
+use uniplate::Uniplate;
+
+use conjure_rule_macros::register_rule_set;
+
+// optimised comprehension expansion for associative-commutative operators
+register_rule_set!("Better_AC_Comprehension_Expansion", ("Base"));
+
+/// expand compatible comprehensions using ac optimisations / Comprehension::expand_ac.
+
+#[register_rule(("Better_AC_Comprehension_Expansion", 2001))]
+fn expand_comprehension_ac(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult {
+    // Is this an ac expression?
+    let ac_operator_kind = expr.to_ac_operator_kind().ok_or(RuleNotApplicable)?;
+
+    debug_assert_eq!(
+        expr.children().len(),
+        1,
+        "AC expressions should have exactly one child."
+    );
+
+    let Expr::Comprehension(_, ref comprehension) = expr.children()[0] else {
+        return Err(RuleNotApplicable);
+    };
+
+    // unwrap comprehensions inside out. This reduces calls to minion when rewriting nested
+    // comprehensions.
+    let nested_comprehensions: VecDeque<Comprehension> =
+        comprehension.clone().return_expression().universe_bi();
+    if !nested_comprehensions.is_empty() {
+        return Err(RuleNotApplicable);
+    };
+
+    // TODO: check what kind of error this throws and maybe panic
+    let mut symbols = symbols.clone();
+    let results = comprehension
+        .clone()
+        .expand_ac(&mut symbols, ac_operator_kind)
+        .or(Err(RuleNotApplicable))?;
+
+    let new_expr = ac_operator_kind.as_expression(into_matrix_expr!(results));
+    Ok(Reduction::with_symbols(new_expr, symbols))
+}
 
 #[register_rule(("Base", 2000))]
 fn expand_comprehension(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult {
     let Expr::Comprehension(_, comprehension) = expr else {
+        return Err(RuleNotApplicable);
+    };
+
+    // unwrap comprehensions inside out. This reduces calls to minion when rewriting nested
+    // comprehensions.
+    let nested_comprehensions: VecDeque<Comprehension> =
+        comprehension.clone().return_expression().universe_bi();
+    if !nested_comprehensions.is_empty() {
         return Err(RuleNotApplicable);
     };
 
@@ -19,33 +72,33 @@ fn expand_comprehension(expr: &Expr, symbols: &SymbolTable) -> ApplicationResult
     let results = comprehension
         .as_ref()
         .clone()
-        .solve_with_minion(&mut symbols)
+        .expand_simple(&mut symbols)
         .or(Err(RuleNotApplicable))?;
 
     Ok(Reduction::with_symbols(into_matrix_expr!(results), symbols))
 }
 
-#[register_rule(("Base", 8000))]
-fn comprehension_move_static_guard_into_generators(
-    expr: &Expr,
-    _: &SymbolTable,
-) -> ApplicationResult {
-    let Expr::Comprehension(_, comprehension) = expr else {
-        return Err(RuleNotApplicable);
-    };
+// #[register_rule(("Base", 8000))]
+// fn comprehension_move_static_guard_into_generators(
+//     expr: &Expr,
+//     _: &SymbolTable,
+// ) -> ApplicationResult {
+//     let Expr::Comprehension(_, comprehension) = expr else {
+//         return Err(RuleNotApplicable);
+//     };
 
-    let mut comprehension = comprehension.clone();
-    let Expr::Imply(_, guard, expr) = comprehension.clone().return_expression() else {
-        return Err(RuleNotApplicable);
-    };
+//     let mut comprehension = comprehension.clone();
+//     let Expr::Imply(_, guard, expr) = comprehension.clone().return_expression() else {
+//         return Err(RuleNotApplicable);
+//     };
 
-    if comprehension.add_induction_guard(*guard) {
-        comprehension.replace_return_expression(*expr);
-        Ok(Reduction::pure(Expr::Comprehension(
-            Metadata::new(),
-            comprehension,
-        )))
-    } else {
-        Err(RuleNotApplicable)
-    }
-}
+//     if comprehension.add_induction_guard(*guard) {
+//         comprehension.replace_return_expression(*expr);
+//         Ok(Reduction::pure(Expr::Comprehension(
+//             Metadata::new(),
+//             comprehension,
+//         )))
+//     } else {
+//         Err(RuleNotApplicable)
+//     }
+// }
