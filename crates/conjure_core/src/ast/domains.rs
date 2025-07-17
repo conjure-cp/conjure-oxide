66b48acaa1c599e2414e013b15ebdbb42c03c86f
Niklas Dewally <niklas@dewally.com>
2025-07-17 12:57:26 +0100

    fix(ast): typo in `Range::contains`

--- crates/conjure_core/src/ast/domains.rs
+++ crates/conjure_core/src/ast/domains.rs
@@ -31,8 +31,8 @@ impl<A: Ord> Range<A> {
         match self {
             Range::Single(x) => x == val,
             Range::Bounded(x, y) => x <= val && val <= y,
-            Range::UnboundedR(x) => x <= val,
-            Range::UnboundedL(x) => x >= val,
+            Range::UnboundedR(x) => x >= val,
+            Range::UnboundedL(x) => x <= val,
         }
     }
 }
