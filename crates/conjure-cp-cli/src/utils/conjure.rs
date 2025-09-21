d674cb1bf6a73b6c3e5ed10e0641ded693809730
Tudor Huica <th201@st-andrews.ac.uk>
2025-09-21 19:59:32 +0100

    first impl of creating a dom const

--- crates/conjure-cp-cli/src/utils/conjure.rs
+++ crates/conjure-cp-cli/src/utils/conjure.rs
@@ -18,6 +18,7 @@ use crate::utils::json::sort_json_object;
 use conjure_cp::Model;
 use conjure_cp::parse::tree_sitter::parse_essence_file;
 use conjure_cp::solver::Solver;
+use conjure_cp::rule_engine::rewrite_naive;
 use conjure_cp::solver::adaptors::Minion;
 use conjure_cp::ast::{Atom, Expression, Metadata, Moo};
 use glob::glob;
@@ -33,38 +34,141 @@ pub fn get_minion_solutions_dominance(
 ) -> Result<Vec<BTreeMap<Name, Literal>>, anyhow::Error> {
 
     match model.clone().dominance {
-    Some(_) => {
+    Some(d) => {
+        
+        // Vector which stores all non-dominated solutions
         let mut res: Vec<BTreeMap<Name,Literal>> = Vec::new();
+        let mut i = 1;
 
-        let atomic1 = Expression::Atomic(Metadata::new(), Atom::Literal(Literal::Int(2)));
-        let variabke = model.get_var(&Name::user("cost"));
-        let variabke_decl = match variabke {
-            Some(decl) => decl,
-            None => {
-                // Handle the error or panic
-                panic!("Variable 'cost' not found in symbol table");
-            }   
-        };
-
-        let dummy_expr = Expression::Eq(
-            Metadata::new(),
-            Moo::new(Expression::Atomic(Metadata::new(), Atom::new_ref(variabke_decl))),
-            Moo::new(atomic1)
-        );
-
-        let subModel = model.add_constraint(dummy_expr);
-        
-        println!("{}",model);
-        for _i in 0..12 {
+        // Keep looking for non-dominated solutions
+        loop {
+            // Find first solution
             let solution = get_minion_solutions(model.clone(), 1, solver_input_file)?;
-            res.extend(solution);
+            
+            // If we don't have any left then exit
+            if solution.len()==0
+            {
+                break;
+            }
+
+            // Add solution to the list
+            res.extend(solution.clone());
+
+            if let Expression::DominanceRelation(_, inner_expr) = d {
+
+                let new_blocking_constraint = crate_blocking_constraint_from_solution(&inner_expr, solution[0].clone());
+
+                println!("{:?}", new_blocking_constraint);
+            }
+
+            break;
+
+            // Create new blocking constraint
+            let atomic1 = Expression::Atomic(Metadata::new(), Atom::Literal(Literal::Int(i)));i+=1;
+            let variabke = model.get_var(&Name::user("cost"));
+            let variabke_decl = match variabke {
+                Some(decl) => decl,
+                None => {
+                    // Handle the error or panic
+                    panic!("Variable 'cost' not found in symbol table");
+                }   
+            };
+            let dummy_expr = Expression::Gt(
+                Metadata::new(),
+                Moo::new(Expression::Atomic(Metadata::new(), Atom::new_ref(variabke_decl))),
+                Moo::new(atomic1)
+            );
+            
+            // Add the new blocking constraint
+            model.add_constraint(dummy_expr);
+            let rule_sets = model.context.read().unwrap().rule_sets.clone();
+            // TODO: not sure of the bools
+            model = rewrite_naive(
+                &model,
+                &rule_sets,
+                false,
+                false,
+            )?;
+            println!("{}",model);
         }
-        
         return Ok(res);
     },
     None => get_minion_solutions(model.clone(),num_sols,solver_input_file),
     }
 }
+
+pub fn crate_blocking_constraint_from_solution(
+    expr: &Expression,
+    solution: BTreeMap<Name, Literal>,
+    // TODO: change from option
+) -> Option<Expression> {
+println!("cou");
+
+    // TODO: could also change FromSolution to take a name and not an expression
+    match expr {
+        Expression::FromSolution(_, name) => {
+            if let Expression::Atomic(_, Atom::Reference(ptr)) = &**name {
+            let var_name = ptr.name();
+
+            return solution.get(&var_name).map(|value| {
+                Expression::Atomic(Metadata::new(), Atom::Literal(value.clone()))
+            });
+            }
+        
+            return None;
+        }
+        Expression::Eq(_, expr1, expr2) |
+        Expression::Lt(_, expr1, expr2) |
+        Expression::Leq(_, expr1, expr2) |
+        Expression::Geq(_, expr1, expr2) |
+        Expression::Gt(_, expr1, expr2) 
+        => {
+            if let (Some(left), Some(right)) = (
+                crate_blocking_constraint_from_solution(expr1, solution.clone()),
+                crate_blocking_constraint_from_solution(expr2, solution.clone()),
+            ) {
+                let left_expr = Moo::new(left);
+                let right_expr = Moo::new(right);
+                let meta = Metadata::new();
+
+                Some(match expr {
+                    Expression::Eq(_, _, _) => Expression::Eq(meta, left_expr, right_expr),
+                    Expression::Lt(_, _, _) => Expression::Lt(meta, left_expr, right_expr),
+                    Expression::Leq(_, _, _) => Expression::Leq(meta, left_expr, right_expr),
+                    Expression::Geq(_, _, _) => Expression::Geq(meta, left_expr, right_expr),
+                    Expression::Gt(_, _, _) => Expression::Gt(meta, left_expr, right_expr),
+                _ => return None,
+        })
+            } else {
+                return None;
+            }
+        }   
+
+        Expression::And(_, expr1) |
+        Expression::Or(_, expr1) 
+        => {
+            if let Some(expression) = crate_blocking_constraint_from_solution(expr1, solution.clone())
+            {
+                let new_expr = Moo::new(expression);
+                let meta = Metadata::new();
+
+                Some(match expr {
+                    Expression::And(_, _) => Expression::And(meta, new_expr),
+                     Expression::Or(_, _) => Expression::Or(meta, new_expr),
+                    
+                _ => return None,
+            })
+            } else {
+                return None;
+            }
+        }   
+
+        _ => Some(expr.clone()),
+    }
+
+
+}
+
 
 
 pub fn get_minion_solutions(
