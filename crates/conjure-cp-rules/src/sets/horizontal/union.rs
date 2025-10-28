01c05f58a6b27ad79df1fcf637d94a1f12c0c71f
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2025-10-28 11:08:11 +0000

    style: format new lines

--- crates/conjure-cp-rules/src/sets/horizontal/union.rs
+++ crates/conjure-cp-rules/src/sets/horizontal/union.rs
@@ -10,27 +10,25 @@ use conjure_cp::rule_engine::{
 #[register_rule(("Base", 8700))]
 fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
     match expr {
-        Expr::In(_, a, expr2) => {
-            match &**expr2 {
-                Expr::Union(_, b, c) => {
-                    if let Some(ReturnType::Set(_)) = b.as_ref().return_type() {
-                        if let Some(ReturnType::Set(_)) = c.as_ref().return_type() {
-                            let expr1 = Expr::In(Metadata::new(), a.clone(), b.clone());
-                            let expr2 = Expr::In(Metadata::new(), a.clone(), c.clone());
-                            Ok(Reduction::pure(Expr::Or(
-                                Metadata::new(),
-                                Moo::new(matrix_expr![expr1, expr2]),
-                            )))
-                        } else {
-                            Err(RuleNotApplicable)
-                        }
+        Expr::In(_, a, expr2) => match &**expr2 {
+            Expr::Union(_, b, c) => {
+                if let Some(ReturnType::Set(_)) = b.as_ref().return_type() {
+                    if let Some(ReturnType::Set(_)) = c.as_ref().return_type() {
+                        let expr1 = Expr::In(Metadata::new(), a.clone(), b.clone());
+                        let expr2 = Expr::In(Metadata::new(), a.clone(), c.clone());
+                        Ok(Reduction::pure(Expr::Or(
+                            Metadata::new(),
+                            Moo::new(matrix_expr![expr1, expr2]),
+                        )))
                     } else {
                         Err(RuleNotApplicable)
                     }
+                } else {
+                    Err(RuleNotApplicable)
                 }
-                _ => Err(RuleNotApplicable),
             }
-        }
+            _ => Err(RuleNotApplicable),
+        },
         _ => Err(RuleNotApplicable),
     }
 }
