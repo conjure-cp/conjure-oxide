8988171ba7e262e43e0a9422d0136a2762736582
Leia McAlister-Young <l.mcalisteryoung@gmail.com>
2025-10-28 12:45:20 +0000

    feat(grammar): add support for set domains and set operations

--- crates/tree-sitter-essence/grammar.js
+++ crates/tree-sitter-essence/grammar.js
@@ -76,6 +76,7 @@ module.exports = grammar ({
       field("tuple_domain", $.tuple_domain),
       field("matrix_domain", $.matrix_domain),
       field("record_domain", $.record_domain),
+      field("set_domain", $.set_domain),
     ),
     bool_domain: $ => "bool",
 
@@ -121,6 +122,29 @@ module.exports = grammar ({
       "}"
     ),
 
+    set_domain: $ => choice(
+      seq(
+        "set",
+        optional(commaSep1($.set_attribute)),
+        "of",
+        field("value_domain", $.domain)
+      ),
+      $.set_literal
+    ),
+
+    set_attribute: $ => seq(
+      "(",
+      field("attribute", choice("size", "minSize", "maxSize")),
+      field("attribute_value", $.integer),
+      ")"
+    ),
+
+    set_literal: $ => seq(
+      "{",
+      field("element", commaSep1($.arithmetic_expr)),
+      "}"
+    ),
+
     name_domain_pair: $ => seq(
       field("name", $.identifier),
       ":",
@@ -147,6 +171,7 @@ module.exports = grammar ({
       field("iff_expr", $.iff_expr),
       field("quantifier_expression_bool", $.quantifier_expr_bool),
       field("sub_bool_expression", $.sub_bool_expr),
+      field("set_operation_bool", $.set_operator_bool)
     )),
 
     not_expr: $ => prec(20, seq("!", field("expression", choice($.bool_expr, $.comparison_expr, $.atom)))),
@@ -198,6 +223,12 @@ module.exports = grammar ({
     ))),
 
     sub_bool_expr: $ => prec(1, seq("(", field("expression", choice($.bool_expr, $.comparison_expr)), ")")),
+
+    set_operation_bool: $ => seq(
+      field("left", $.atom),
+      field("operator", choice("in", "subset", "subsetEq", "supset", "supsetEq")),
+      field("right", $.atom)
+    ),
     
     arithmetic_expr: $ => prec(3, choice(
       field("atom", $.atom),
@@ -220,6 +251,8 @@ module.exports = grammar ({
       field("record", $.record),
       field("from_solution", $.from_solution),
       field("tuple_matrix_record_index_or_slice", $.tuple_matrix_record_index_or_slice),
+      field("set_literal", $.set_literal),
+      field("set_operation", $.set_operation),
     )),
 
     tuple: $ => prec(-5, seq(
@@ -259,6 +292,19 @@ module.exports = grammar ({
       field("indices", $.indices),
       "]"
     ),
+
+    set_operation: $ => choice($.binary_set_operation, $.unary_set_operation),
+
+    binary_set_operation: $ => prec.left(seq(
+      field("left", $.atom),
+      choice("union", "intersect"),
+      field("right", $.atom)
+    )),
+
+    unary_set_operation: $ => prec(1,seq(
+      "powerSet",
+      field("argument", $.atom)
+    )),
 
     indices: $ => commaSep1(choice(field("index", $.arithmetic_expr), field("null_index", $.null_index))),
 
