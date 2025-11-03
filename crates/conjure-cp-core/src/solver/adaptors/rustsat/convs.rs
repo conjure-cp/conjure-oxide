6340f2c0031573fe6ae02633c99e025acb40c107
Shikhar Srivastava <109688678+Shikhar-Srivastava-16@users.noreply.github.com>
2025-11-03 22:23:46 +0000

    feat: Unbound Literals (alt) (#792)
    Introducing changes to the SAT adaptor in order to call the CNF better.
    
    Adds support for unconstrained variables based on the suggestion by Dr
    Ozgur Akgun in Issue
    https://github.com/conjure-cp/conjure-oxide/issues/705 "Default Setting
    for Variables".
    
    Here, the solving process (abstractly speaking) generates a solution,
    adds all unbound literals as DontCares, then enumerates over all
    combinations for the DontCares.
    
    As such, the result here is that for each solution generated by the
    solver, the adaptor will run a function to produce a vector of $2^n$
    solutions, with $n$ being the number of DontCares in the generated
    solution.
    
    Mutually exclusive with pull request #778, _'Feature: Introducing
    changes to the SAT adaptor in to expand solving'_.
    Awaiting intended benchmarking tests.
    
    ---------
    
    Co-authored-by: Hecent <74259874+hecent@users.noreply.github.com>
    Co-authored-by: Shikhar Srivastava <ss504@st-andrews.ac.uk>

--- crates/conjure-cp-core/src/solver/adaptors/rustsat/convs.rs
+++ crates/conjure-cp-core/src/solver/adaptors/rustsat/convs.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashMap, env::Vars, io::Lines};
+use core::panic;
+use std::{
+    collections::HashMap,
+    env::{Vars, vars},
+    io::Lines,
+};
 
 use rustsat::{
     clause,
@@ -13,6 +18,7 @@ use anyhow::{Result, anyhow};
 
 use crate::{
     ast::{CnfClause, Expression, Moo, Name},
+    bug,
     solver::Error,
 };
 
@@ -28,7 +34,7 @@ pub fn handle_lit(
         // not literal
         Expression::Not(_, _) => handle_not(l1, vars_added, inst),
 
-        _ => panic!("Literal expected"),
+        _ => todo!("Literal expected"),
     }
 }
 
@@ -43,7 +49,7 @@ pub fn handle_not(
             let a = Moo::unwrap_or_clone(ref_a);
             handle_atom(a, false, vars_added, inst)
         }
-        _ => panic!("Not Expected"),
+        _ => todo!("Not Expression Expected"),
     }
 }
 
@@ -94,18 +100,26 @@ pub fn handle_disjn(
         lits.add(lit);
     }
 
-    // literal => {
-    // let lit: Lit = handle_lit(literal, vars_added, inst_in_use);
-    // lits.add(lit);
-
     inst_in_use.add_clause(lits);
 }
 
-pub fn handle_cnf(vec_cnf: &Vec<CnfClause>, vars_added: &mut HashMap<Name, Lit>) -> SatInstance {
+pub fn handle_cnf(
+    vec_cnf: &Vec<CnfClause>,
+    vars_added: &mut HashMap<Name, Lit>,
+    finds: Vec<Name>,
+) -> SatInstance {
     let mut inst = SatInstance::new();
+
+    tracing::info!("{:?} are all the decision vars found.", finds);
+
+    for name in finds {
+        vars_added.insert(name, inst.new_lit());
+    }
+
     for disjn in vec_cnf {
         handle_disjn(disjn, vars_added, &mut inst);
     }
+
     inst
 }
 
