61b2df23ed2458c4c2908f96182b3b21d7a7379e
Leia McAlister-Young <l.mcalisteryoung@gmail.com>
2025-11-03 22:29:03 +0000

    feat(parser): add comprehensions to parser (#1165)
    ## Description
    This PR adds support for comprehensions to the grammar and parser. Since
    it is a more complicated addition than most, I made a new file
    `comprehension.rs` that parses the comprehensions.
    
    ## Related Issues
    #1065 
    Includes changes from PR #1154, which adds sets.
    
    ## Key Changes
    - Add support for comprehensions to grammar
    - Add `comprehension.rs` file to parse comprehensions
    - Change all functions in crate to pass
    `Option<&Rc<RefCell<SymbolTable>>>` instead of `Option<&SymbolTable>`

--- crates/conjure-cp-essence-parser/src/parser/abstract_literal.rs
+++ crates/conjure-cp-essence-parser/src/parser/abstract_literal.rs
@@ -4,18 +4,20 @@ use crate::util::named_children;
 use crate::{EssenceParseError, field};
 use conjure_cp_core::ast::{AbstractLiteral, Domain, Expression, SymbolTable};
 use conjure_cp_core::{domain_int, range};
+use std::cell::RefCell;
+use std::rc::Rc;
 use tree_sitter::Node;
 
 pub fn parse_abstract(
     node: &Node,
     source_code: &str,
-    symbols: Option<&SymbolTable>,
+    symbols_ptr: Option<Rc<RefCell<SymbolTable>>>,
 ) -> Result<AbstractLiteral<Expression>, EssenceParseError> {
     match node.kind() {
-        "record" => parse_record(node, source_code, symbols),
-        "tuple" => parse_tuple(node, source_code, symbols),
-        "matrix" => parse_matrix(node, source_code, symbols),
-        "set_literal" => parse_set_literal(node, source_code, symbols),
+        "record" => parse_record(node, source_code, symbols_ptr),
+        "tuple" => parse_tuple(node, source_code, symbols_ptr),
+        "matrix" => parse_matrix(node, source_code, symbols_ptr),
+        "set_literal" => parse_set_literal(node, source_code, symbols_ptr),
         _ => Err(EssenceParseError::syntax_error(
             format!("Expected abstract literal, got: '{}'", node.kind()),
             Some(node.range()),
@@ -26,7 +28,7 @@ pub fn parse_abstract(
 fn parse_record(
     node: &Node,
     source_code: &str,
-    symbols: Option<&SymbolTable>,
+    symbols_ptr: Option<Rc<RefCell<SymbolTable>>>,
 ) -> Result<AbstractLiteral<Expression>, EssenceParseError> {
     let mut values = Vec::new();
     for child in node.children_by_field_name("name_value_pair", &mut node.walk()) {
@@ -34,8 +36,12 @@ fn parse_record(
         let name_str = &source_code[name_node.start_byte()..name_node.end_byte()];
         let name = conjure_cp_core::ast::Name::user(name_str);
 
-        let value: Expression =
-            parse_expression(field!(child, "value"), source_code, node, symbols)?;
+        let value: Expression = parse_expression(
+            field!(child, "value"),
+            source_code,
+            node,
+            symbols_ptr.clone(),
+        )?;
         values.push(conjure_cp_core::ast::records::RecordValue { name, value });
     }
     Ok(AbstractLiteral::Record(values))
@@ -44,11 +50,16 @@ fn parse_record(
 fn parse_tuple(
     node: &Node,
     source_code: &str,
-    symbols: Option<&SymbolTable>,
+    symbols_ptr: Option<Rc<RefCell<SymbolTable>>>,
 ) -> Result<AbstractLiteral<Expression>, EssenceParseError> {
     let mut elements = Vec::new();
     for child in named_children(node) {
-        elements.push(parse_expression(child, source_code, node, symbols)?);
+        elements.push(parse_expression(
+            child,
+            source_code,
+            node,
+            symbols_ptr.clone(),
+        )?);
     }
     Ok(AbstractLiteral::Tuple(elements))
 }
@@ -56,13 +67,18 @@ fn parse_tuple(
 fn parse_matrix(
     node: &Node,
     source_code: &str,
-    symbols: Option<&SymbolTable>,
+    symbols_ptr: Option<Rc<RefCell<SymbolTable>>>,
 ) -> Result<AbstractLiteral<Expression>, EssenceParseError> {
     let mut elements = vec![];
     let mut domain: Option<Domain> = None;
     for child in named_children(node) {
         if child.kind() == "arithmetic_expr" {
-            elements.push(parse_expression(child, source_code, node, symbols)?);
+            elements.push(parse_expression(
+                child,
+                source_code,
+                node,
+                symbols_ptr.clone(),
+            )?);
         } else {
             domain = Some(parse_domain(child, source_code)?);
         }
@@ -78,11 +94,16 @@ fn parse_matrix(
 fn parse_set_literal(
     node: &Node,
     source_code: &str,
-    symbols: Option<&SymbolTable>,
+    symbols_ptr: Option<Rc<RefCell<SymbolTable>>>,
 ) -> Result<AbstractLiteral<Expression>, EssenceParseError> {
     let mut elements = Vec::new();
     for child in named_children(node) {
-        elements.push(parse_expression(child, source_code, node, symbols)?);
+        elements.push(parse_expression(
+            child,
+            source_code,
+            node,
+            symbols_ptr.clone(),
+        )?);
     }
     Ok(AbstractLiteral::Set(elements))
 }
