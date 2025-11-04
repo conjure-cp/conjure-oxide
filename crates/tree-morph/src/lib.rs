d6feb8335f8dd72befebcbe82d0a2def8bedb9fd
Felix Leitner <52215017+lixitrixi@users.noreply.github.com>
2025-11-04 07:43:14 +0000

    feat(treemorph): add engine type and builder (#1000)
    This is a WIP I'm posting here since I have little time to work on over
    the next week. Pinging @niklasdewally in case you haven't already
    started something similar!
    
    EDIT:
    
    This PR changes the API of treemorph to a configurable `Engine` type,
    constructed via an `EngineBuilder`. Rule groups can be added to the
    engine as well as handlers for events corresponding with Zipper movement
    around the tree. The events are potentially useful for scope handling in
    Conjure Oxide.
    
    A rule selector can also be chosen for the whole engine. Overall the
    interface is similar to the original `morph` function, but allows
    cleaner construction and room for growth in the API (new inputs can be
    added without a breaking change).
    
    ---------
    
    Co-authored-by: Arnav Bista <53308039+Arnav-Bista@users.noreply.github.com>
    Co-authored-by: Özgür Akgün <ozgurakgun@gmail.com>

--- crates/tree-morph/src/lib.rs
+++ crates/tree-morph/src/lib.rs
@@ -3,6 +3,8 @@
 
 pub mod commands;
 pub mod engine;
+pub mod engine_builder;
+mod events;
 pub mod helpers;
 pub mod rule;
 mod update;
@@ -10,7 +12,8 @@ mod update;
 /// Re-exported functions and types for convenience.
 pub mod prelude {
     pub use crate::commands::Commands;
-    pub use crate::engine::morph;
+    pub use crate::engine::Engine;
+    pub use crate::engine_builder::EngineBuilder;
     pub use crate::helpers::select_first;
     pub use crate::rule::{Rule, RuleFn};
     pub use crate::rule_fns;
