8fb0633be7f0ab22cf11c49203b26f8deaaaba1d
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2025-11-07 13:44:30 +0000

    union comprehension rule

--- crates/conjure-cp-rules/src/sets/horizontal/union.rs
+++ crates/conjure-cp-rules/src/sets/horizontal/union.rs
@@ -1,34 +1,75 @@
-use conjure_cp::ast::Metadata;
+use conjure_cp::ast::comprehension::Comprehension;
+use conjure_cp::ast::{DeclarationKind, Metadata};
 use conjure_cp::ast::{Expression as Expr, Moo, ReturnType, SymbolTable, Typeable};
-use conjure_cp::matrix_expr;
 use conjure_cp::rule_engine::Reduction;
 use conjure_cp::rule_engine::{
     ApplicationError::RuleNotApplicable, ApplicationResult, register_rule,
 };
+use conjure_cp::{into_matrix_expr, matrix_expr};
 
-// A in (B union C) -> A in B \/ A in C
+// [ return_expr | i <- A union B, guards...] -> flatten([[ return_expr | i <- A, guards...], [ return_expr | i <- B, !(i in A), guards...]; int(1..2)])
 #[register_rule(("Base", 8700))]
 fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
     match expr {
-        Expr::In(_, a, expr2) => match &**expr2 {
-            Expr::Union(_, b, c) => {
-                if let Some(ReturnType::Set(_)) = b.as_ref().return_type() {
-                    if let Some(ReturnType::Set(_)) = c.as_ref().return_type() {
-                        let expr1 = Expr::In(Metadata::new(), a.clone(), b.clone());
-                        let expr2 = Expr::In(Metadata::new(), a.clone(), c.clone());
-                        Ok(Reduction::pure(Expr::Or(
-                            Metadata::new(),
-                            Moo::new(matrix_expr![expr1, expr2]),
-                        )))
-                    } else {
-                        Err(RuleNotApplicable)
+        Expr::Comprehension(_, comp) => {
+            // find if any of the generators are generating from expressions
+            for (name, decl) in comp.generator_submodel.symbols().clone().into_iter_local() {
+                if let DeclarationKind::ElementOf(gen_expr) = decl.kind() {
+                    // match on expression being of form A union B
+                    match gen_expr {
+                        Expr::Union(_, a, b) => {
+                            // original comprehension return expression
+                            let return_expr = comp.return_expression();
+
+                            // extract guards from original comprehension
+                            let guards = comp.generator_submodel.constraints();
+
+                            // create [ return_expr | i <- A, guards...] part
+                            let mut comprehension1 =
+                                ComprehensionBuilder::new(Rc::new(RefCell::new(symbols.clone())));
+                            let i1 = comprehension1
+                                .generator_symboltable()
+                                .borrow_mut()
+                                .gensym(&a);
+                            comprehension1 = comprehension1.generator(i1);
+                            for guard in guards.clone() {
+                                comprehension1 = comprehension1.guard(guard);
+                            }
+
+                            // create [ return_expr | i <- B, !(i in A), guards...] part
+                            let mut comprehension2 =
+                                ComprehensionBuilder::new(Rc::new(RefCell::new(symbols.clone())));
+                            let i2 = comprehension2
+                                .generator_symboltable()
+                                .borrow_mut()
+                                .gensym(&b);
+                            comprehension2 = comprehension2.generator(i1);
+                            comprehension2 = comprehension2.guard(Expr::Not(
+                                Metadata::new(),
+                                Expr::In(Metadata::new(), i2, Moo::new(b.clone())),
+                            ));
+                            for guard in guards.clone() {
+                                comprehension2 = comprehension2.guard(guard);
+                            }
+
+                            return Ok(Reduction::pure(Expr::Comprehension(
+                                Metadata::new(),
+                                Moo::new(Expr::Flatten(
+                                    Metadata::new(),
+                                    Moo::new(), // <-- don't know what do do about the fact flatten can take 1 or 2 arguments
+                                    Moo::new(
+                                        into_matrix_expr!(vec![comprehension1, comprehension2]; domain_int!(1..2)),
+                                    ),
+                                )),
+                            )));
+                        }
+                        _ => Err(RuleNotApplicable),
                     }
-                } else {
-                    Err(RuleNotApplicable)
                 }
             }
-            _ => Err(RuleNotApplicable),
-        },
+
+            Err(RuleNotApplicable)
+        }
         _ => Err(RuleNotApplicable),
     }
 }
