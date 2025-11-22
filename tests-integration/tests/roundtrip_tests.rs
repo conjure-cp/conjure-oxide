b47ef123d3b1584f6a3dce48877ded2bb87a2fc3
Nicholas Davidson <nd65@st-andrews.ac.uk>
2025-11-22 18:35:33 +0000

    feat: allows roundtrip test to also check valid errors

--- tests-integration/tests/roundtrip_tests.rs
+++ tests-integration/tests/roundtrip_tests.rs
@@ -1,16 +1,15 @@
-use conjure_cp::parse::tree_sitter::parse_essence_file;
-use conjure_cp::context::Context;
-use conjure_cp_cli::utils::testing::{
-    read_model_json, save_model_json
-};
 use conjure_cp::Model;
+use conjure_cp::context::Context;
+use conjure_cp::parse::tree_sitter::EssenceParseError;
+use conjure_cp::parse::tree_sitter::parse_essence_file;
+use conjure_cp_cli::utils::testing::{read_model_json, save_model_json};
 
 use std::env;
+use std::error::Error;
+use std::fs;
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::RwLock;
-use std::error::Error;
-use std::fs;
 
 use std::io::Write;
 
@@ -19,61 +18,105 @@ use std::io::Write;
 fn roundtrip_test(path: &str, filename: &str, extension: &str) -> Result<(), Box<dyn Error>> {
     /*
     Parses Essence file
-    Saves generated AST model JSON
-    Saves generated Essence
+     | If valid
+        Saves generated AST model JSON
+        Saves generated Essence
 
-    Compares expected and generated AST model JSON
-    Compares expected and generated Essence
+        Compares expected and generated AST model JSON
+        Compares expected and generated Essence
 
-    Parses generated Essence back to being a model
-    Saves new model as Essence (generated2)
-    Compare initally generated Essence with newly generated Essence
+        Parses generated Essence back to being a model
+        Saves new model as Essence (generated2)
+        Compare initally generated Essence with newly generated Essence
+
+    | If invalid
+        Saves EssenceParseError
+        Compares expected and generated errors
     */
 
     let accept = env::var("ACCEPT").unwrap_or("false".to_string()) == "true";
 
     let file_path = format!("{path}/{filename}.{extension}");
     let context: Arc<RwLock<Context<'static>>> = Default::default();
-    
-    let initial_model = parse_essence_file(&file_path, context.clone())?;
-    save_model_json(&initial_model, path, filename, "parse")?;
-    save_essence(&initial_model, path, filename, "generated")?;
 
-    // When ACCEPT = true, copy over generated to expected
-    if accept {
-        std::fs::copy(
-            format!("{path}/{filename}.generated-parse.serialised.json"),
-            format!("{path}/{filename}.expected-parse.serialised.json"),
-        )?;
-        std::fs::copy(
-            format!("{path}/{filename}.generated-essence.essence"),
-            format!("{path}/{filename}.expected-essence.essence"),
-        )?;
+    let initial_parse = parse_essence_file(&file_path, context.clone());
+    match initial_parse {
+        Ok(initial_model) => {
+            save_model_json(&initial_model, path, filename, "parse")?;
+            save_essence(&initial_model, path, filename, "generated")?;
+
+            // When ACCEPT = true, copy over generated to expected
+            if accept {
+                std::fs::copy(
+                    format!("{path}/{filename}.generated-parse.serialised.json"),
+                    format!("{path}/{filename}.expected-parse.serialised.json"),
+                )?;
+                std::fs::copy(
+                    format!("{path}/{filename}.generated-essence.essence"),
+                    format!("{path}/{filename}.expected-essence.essence"),
+                )?;
+            }
+
+            // Ensures ACCEPT=true has been run at least once
+            if !accept
+                && !Path::new(&format!("{path}/{filename}.expected-parse.serialised.json")).exists()
+            {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::NotFound,
+                    format!("Expected output file not found: Run with ACCEPT=true"),
+                )));
+            }
+
+            // Compare the expected and generated model
+            let expected_model = read_model_json(&context, path, filename, "expected", "parse")?;
+            let generated_model = read_model_json(&context, path, filename, "generated", "parse")?;
+            assert_eq!(generated_model, expected_model);
+
+            // Compares essence files
+            let expected_essence =
+                fs::read_to_string(&format!("{path}/{filename}.expected-essence.essence"))?;
+            let generated_essence =
+                fs::read_to_string(&format!("{path}/{filename}.generated-essence.essence"))?;
+            assert_eq!(expected_essence, generated_essence);
+
+            // Compares roundtrip
+            let new_model = parse_essence_file(
+                &format!("{path}/{filename}.generated-essence.essence"),
+                context.clone(),
+            )?;
+            save_essence(&new_model, path, filename, "generated2")?;
+            let new_generated_essence =
+                fs::read_to_string(&format!("{path}/{filename}.generated2-essence.essence"))?;
+            assert_eq!(generated_essence, new_generated_essence);
+        }
+
+        Err(parse_error) => {
+            println!("MADE IT HERE?");
+            save_parse_error(&parse_error, path, filename, "generated")?;
+
+            // When ACCEPT = true, copy over generated to expected
+            if accept {
+                std::fs::copy(
+                    format!("{path}/{filename}.generated-error.txt"),
+                    format!("{path}/{filename}.expected-error.txt"),
+                )?;
+            }
+
+            // Ensures ACCEPT=true has been run at least once
+            if !accept && !Path::new(&format!("{path}/{filename}.expected-error.txt")).exists() {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::NotFound,
+                    format!("Expected output file not found: Run with ACCEPT=true"),
+                )));
+            }
+
+            let expected_error =
+                fs::read_to_string(&format!("{path}/{filename}.expected-error.txt"))?;
+            let generated_error =
+                fs::read_to_string(&format!("{path}/{filename}.generated-error.txt"))?;
+            assert_eq!(expected_error, generated_error);
+        }
     }
-
-    // Ensures ACCEPT=true has been run at least once
-    if !Path::new(&format!("{path}/{filename}.expected-parse.serialised.json")).exists(){
-        return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            format!("Expected output file not found: Run with ACCEPT=true"),
-        )));
-    }
-
-    // Compare the expected and generated model
-    let expected_model = read_model_json(&context, path, filename, "expected", "parse")?;
-    let generated_model = read_model_json(&context, path, filename, "generated", "parse")?;
-    assert_eq!(generated_model, expected_model);
-
-    // Compares essence files
-    let expected_essence = fs::read_to_string(&format!("{path}/{filename}.expected-essence.essence"))?;
-    let generated_essence = fs::read_to_string(&format!("{path}/{filename}.generated-essence.essence"))?;
-    assert_eq!(expected_essence,generated_essence);
-
-    // Compares roundtrip
-    let new_model = parse_essence_file(&format!("{path}/{filename}.generated-essence.essence"), context.clone())?;
-    save_essence(&new_model, path, filename, "generated2")?;
-    let new_generated_essence = fs::read_to_string(&format!("{path}/{filename}.generated2-essence.essence"))?;
-    assert_eq!(generated_essence,new_generated_essence);
 
     Ok(())
 }
@@ -87,7 +130,20 @@ fn save_essence(
 ) -> Result<(), std::io::Error> {
     let filename = format!("{path}/{test_name}.{model_type}-essence.essence");
     let mut file = fs::File::create(&filename)?;
-    write!(file,"{}",model)?;
+    write!(file, "{}", model)?;
+    Ok(())
+}
+
+/* Saves a error message as a text file */
+fn save_parse_error(
+    error: &EssenceParseError,
+    path: &str,
+    test_name: &str,
+    model_type: &str,
+) -> Result<(), std::io::Error> {
+    let filename = format!("{path}/{test_name}.{model_type}-error.txt");
+    let mut file = fs::File::create(&filename)?;
+    write!(file, "{}", error)?;
     Ok(())
 }
 
