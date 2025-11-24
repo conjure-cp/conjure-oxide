9cf5dcd8f6aacdf06bc4a92d12adf1b94b67c535
Felix <felix@felixleitner.com>
2025-11-24 12:07:46 +0000

    support lex constraints through minion

--- crates/conjure-cp-rules/src/lex.rs
+++ crates/conjure-cp-rules/src/lex.rs
@@ -1,4 +1,4 @@
-use conjure_cp::ast::{Expression as Expr, SymbolTable};
+use conjure_cp::ast::{Atom, Expression as Expr, Metadata, Moo, SymbolTable};
 use conjure_cp::rule_engine::{ApplicationError, ApplicationResult, Reduction, register_rule};
 
 use ApplicationError::RuleNotApplicable;
@@ -23,8 +23,35 @@ fn normalise_lex_gt_geq(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
 #[register_rule(("Minion", 2000))]
 fn flatten_lex_lt_leq(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
     let (a, b) = match expr {
-        Expr::LexLt(_, a, b) | Expr::LexGt(_, a, b) => Some((a.unwrap_list(), b.unwrap_list())),
-        _ => None,
+        Expr::LexLt(_, a, b) | Expr::LexLeq(_, a, b) => (
+            Moo::unwrap_or_clone(a.clone())
+                .unwrap_list()
+                .ok_or(RuleNotApplicable)?,
+            Moo::unwrap_or_clone(b.clone())
+                .unwrap_list()
+                .ok_or(RuleNotApplicable)?,
+        ),
+        _ => return Err(RuleNotApplicable),
+    };
+
+    if a.len() != b.len() {
+        return Err(ApplicationError::DomainError);
     }
-    .ok_or(RuleNotApplicable)?;
+
+    let atoms_a: Vec<Atom> = a
+        .into_iter()
+        .map(|e| e.try_into().map_err(|_| RuleNotApplicable))
+        .collect::<Result<Vec<_>, ApplicationError>>()?;
+    let atoms_b: Vec<Atom> = b
+        .into_iter()
+        .map(|e| e.try_into().map_err(|_| RuleNotApplicable))
+        .collect::<Result<Vec<_>, ApplicationError>>()?;
+
+    let new_expr = match expr {
+        Expr::LexLt(..) => Expr::FlatLexLt(Metadata::new(), atoms_a, atoms_b),
+        Expr::LexLeq(..) => Expr::FlatLexLeq(Metadata::new(), atoms_a, atoms_b),
+        _ => unreachable!(),
+    };
+
+    Ok(Reduction::pure(new_expr))
 }
