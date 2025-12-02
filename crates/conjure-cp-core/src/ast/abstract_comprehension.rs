c8beb1721aa688eb9f6ad6dafcc61a099c346239
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2025-12-02 16:51:49 +0000

    switch to declarationptr

--- crates/conjure-cp-core/src/ast/abstract_comprehension.rs
+++ crates/conjure-cp-core/src/ast/abstract_comprehension.rs
@@ -1,17 +1,14 @@
-use super::{Name, SymbolTable};
-use crate::ast::{DeclarationPtr, Domain, Expression, ReturnType, Typeable, ac_operators::ACOperatorKind};
-use serde::{Deserialize, Serialize, RcRefCellAsInner};
+use super::SymbolTable;
+use super::declaration::{DeclarationPtr, serde::DeclarationPtrFull};
+use super::serde::RcRefCellAsInner;
+use crate::ast::{DomainPtr, Expression, ReturnType, Typeable};
+use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
-use std::{
-    cell::RefCell,
-    fmt::{Display, Formatter},
-    ops::Deref,
-    rc::Rc,
-};
-use uniplate::Uniplate;
+use std::fmt::{Display, Formatter};
+use std::{cell::RefCell, hash::Hash, hash::Hasher, rc::Rc};
 
 #[serde_as]
-#[derive(Clone, PartialEq, Eq, Uniplate, Serialize, Deserialize, Debug)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
 pub struct AbstractComprehension {
     pub return_expr: Expression,
     pub qualifiers: Vec<Qualifier>,
@@ -19,20 +16,63 @@ pub struct AbstractComprehension {
     pub symbols: Rc<RefCell<SymbolTable>>,
 }
 
-#[derive(Clone, PartialEq, Eq, Uniplate, Serialize, Deserialize, Debug)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug, Hash)]
 pub enum Qualifier {
     Generator(Generator),
     Condition(Expression),
-    ComprehensionLetting(Name, Expression),
+    ComprehensionLetting(ComprehensionLetting),
 }
 
-#[derive(Clone, PartialEq, Eq, Uniplate, Serialize, Deserialize, Debug)]
+#[serde_as]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug, Hash)]
+pub struct ComprehensionLetting {
+    #[serde_as(as = "DeclarationPtrFull")]
+    decl: DeclarationPtr,
+    expression: Expression,
+}
+
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug, Hash)]
 pub enum Generator {
-    DomainGenerator(Name, Domain),
-    ExpressionGenerator(Name, Expression),
+    DomainGenerator(DomainGenerator),
+    ExpressionGenerator(ExpressionGenerator),
 }
 
-pub struct AbstractComprehensionBuilder{
+#[serde_as]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug, Hash)]
+pub struct DomainGenerator {
+    #[serde_as(as = "DeclarationPtrFull")]
+    decl: DeclarationPtr,
+}
+
+#[serde_as]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug, Hash)]
+pub struct ExpressionGenerator {
+    #[serde_as(as = "DeclarationPtrFull")]
+    decl: DeclarationPtr,
+    expression: Expression,
+}
+
+impl AbstractComprehension {
+    pub fn domain_of(&self) -> Option<DomainPtr> {
+        self.return_expr.domain_of()
+    }
+}
+
+impl Typeable for AbstractComprehension {
+    fn return_type(&self) -> ReturnType {
+        self.return_expr.return_type()
+    }
+}
+
+impl Hash for AbstractComprehension {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.symbols.borrow().hash(state);
+        self.return_expr.hash(state);
+        self.qualifiers.hash(state);
+    }
+}
+
+pub struct AbstractComprehensionBuilder {
     pub qualifiers: Vec<Qualifier>,
     pub symbols: Rc<RefCell<SymbolTable>>,
 }
@@ -40,82 +80,69 @@ pub struct AbstractComprehensionBuilder{
 //this is the method that allows you to build an abstract comprehension
 impl AbstractComprehensionBuilder {
     // this method creates an abstract comprehension builder with:
-        // a symbol table
-        // empty list of qualifiers
-        // and no return exp yet -- that will be added in the with_return_value method
-    pub fn new(
-        symbols: Rc<RefCell<SymbolTable>>,
-    ) -> Self {
+    // a symbol table
+    // empty list of qualifiers
+    // and no return exp yet -- that will be added in the with_return_value method
+    pub fn new(symbols: Rc<RefCell<SymbolTable>>) -> Self {
         Self {
             qualifiers: vec![],
-            symbols: Rc::new(RefCell::new(SymbolTable::with_parent(
-                symbol_table_ptr,)))
+            symbols: symbols,
         }
     }
 
-    // TODO: figure out how to return a domain when this is dependent on the final result of the comprehension
-    // Potentially unresolved domain?? tbd
-    pub fn domain_of(&self) -> Option<Domain> {
-        self.return_expr.domain_of()
-    }
+    pub fn new_domain_generator(&mut self, domain: DomainPtr) -> DeclarationPtr {
+        let generator_decl = self.symbols.borrow_mut().gensym(&domain);
 
-    pub fn new_domain_generator(&mut self, domain: Domain) -> Name {
-        let name = self
-            .symbols
-            .borrow_mut()
-            .gensym(&domain)
-            .name()
-            .deref()
-            .to_owned();
-        self.add_domain_generator(name.clone(), domain);
-        name
-    }
-
-    pub fn add_domain_generator(&mut self, name: Name, domain: Domain) {
-        self.symbols
-            .borrow_mut()
-            .insert(DeclarationPtr::new_var(name.clone(), domain.clone()));
         self.qualifiers
             .push(Qualifier::Generator(Generator::DomainGenerator(
-                name, domain,
+                DomainGenerator {
+                    decl: generator_decl.clone(),
+                },
             )));
+
+        generator_decl
     }
 
-    pub fn new_expression_generator(&mut self, expr: Expression) -> Name {
-        let name = self
+    pub fn new_expression_generator(&mut self, expr: Expression) -> DeclarationPtr {
+        let generator_decl = self
             .symbols
             .borrow_mut()
-            .gensym(&expr.domain_of().expect("Expression must have a domain"))
-            .name()
-            .deref()
-            .to_owned();
-        self.add_expression_generator(name.clone(), expr);
-        name
-    }
+            .gensym(&expr.domain_of().expect("Expression must have a domain"));
 
-    pub fn add_expression_generator(&mut self, name: Name, expr: Expression) {
-        self.symbols.borrow_mut().insert(DeclarationPtr::new_var(
-            name.clone(),
-            expr.domain_of().unwrap(),
-        ));
         self.qualifiers
             .push(Qualifier::Generator(Generator::ExpressionGenerator(
-                name, expr,
+                ExpressionGenerator {
+                    decl: generator_decl.clone(),
+                    expression: expr,
+                },
             )));
+
+        generator_decl
     }
 
     //this is the same as the add guard method
     pub fn add_condition(&mut self, condition: Expression) {
-        if condition.return_type() != Some(ReturnType::Bool) {
+        if condition.return_type() != ReturnType::Bool {
             panic!("Condition expression must have boolean return type");
         }
 
         self.qualifiers.push(Qualifier::Condition(condition));
     }
 
-    pub fn add_letting(&mut self, name: Name, expr: Expression) {
+    pub fn new_letting(&mut self, expression: Expression) -> DeclarationPtr {
+        let letting_decl = self.symbols.borrow_mut().gensym(
+            &expression
+                .domain_of()
+                .expect("Expression must have a domain"),
+        );
+
         self.qualifiers
-            .push(Qualifier::ComprehensionLetting(name, expr));
+            .push(Qualifier::ComprehensionLetting(ComprehensionLetting {
+                decl: letting_decl.clone(),
+                expression: expression,
+            }));
+
+        letting_decl
     }
 
     //the lack of the generator_symboltable and return_expr_symboltable
@@ -123,14 +150,11 @@ impl AbstractComprehensionBuilder {
     // 2. it is unclear why there would be a need to access each one uniquely
 
     // TODO: make a pub fn with_return_value :)
-    pub fn with_return_value(
-        self, 
-        mut expression: Expression,
-    ) -> AbstractComprehension {
+    pub fn with_return_value(self, expression: Expression) -> AbstractComprehension {
         AbstractComprehension {
-            return_expr: expression,   
+            return_expr: expression,
             qualifiers: self.qualifiers,
-            symbols: self.symbols,          
+            symbols: self.symbols,
         }
     }
 }
@@ -155,7 +179,9 @@ impl Display for Qualifier {
         match self {
             Qualifier::Generator(generator) => generator.fmt(f),
             Qualifier::Condition(condition) => condition.fmt(f),
-            Qualifier::ComprehensionLetting(name, expr) => {
+            Qualifier::ComprehensionLetting(comp_letting) => {
+                let name = comp_letting.decl.name();
+                let expr = &comp_letting.expression;
                 write!(f, "letting {} = {}", name, expr)
             }
         }
@@ -165,11 +191,14 @@ impl Display for Qualifier {
 impl Display for Generator {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Generator::DomainGenerator(name, domain) => {
+            Generator::DomainGenerator(DomainGenerator { decl }) => {
+                let name = decl.name();
+                let domain = decl.domain().unwrap();
                 write!(f, "{} : {}", name, domain)
             }
-            Generator::ExpressionGenerator(name, expr) => {
-                write!(f, "{} <- {}", name, expr)
+            Generator::ExpressionGenerator(ExpressionGenerator { decl, expression }) => {
+                let name = decl.name();
+                write!(f, "{} <- {}", name, expression)
             }
         }
     }
