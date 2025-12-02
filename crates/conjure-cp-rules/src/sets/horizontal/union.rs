a1a5ae3c2da55722222a3d7a67703e57f161414b
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2025-12-02 17:48:38 +0000

    rewrite union for abstract comprehension

--- crates/conjure-cp-rules/src/sets/horizontal/union.rs
+++ crates/conjure-cp-rules/src/sets/horizontal/union.rs
@@ -1,75 +1,79 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use conjure_cp::ast::comprehension::{Comprehension, ComprehensionBuilder};
-use conjure_cp::ast::{Atom, DeclarationKind, Metadata};
-use conjure_cp::ast::{Expression as Expr, Moo, ReturnType, SymbolTable, Typeable};
-use conjure_cp::rule_engine::Reduction;
+use conjure_cp::ast::abstract_comprehension::{
+    ExpressionGenerator, Generator, Qualifier,
+};
+use conjure_cp::ast::{Atom, Metadata};
+use conjure_cp::ast::{Expression as Expr, Moo, SymbolTable};
 use conjure_cp::rule_engine::{
     ApplicationError::RuleNotApplicable, ApplicationResult, register_rule,
 };
-use conjure_cp::{into_matrix_expr, matrix_expr};
+use conjure_cp::rule_engine::{Reduction};
+use conjure_cp::{into_matrix_expr};
 
-// [ return_expr | i <- A union B, guards...] -> flatten([[ return_expr | i <- A, guards...], [ return_expr | i <- B, !(i in A), guards...]; int(1..2)])
+// [ return_expr | i <- A union B, qualifiers...] -> flatten([[ return_expr | i <- A, qualifiers...], [ return_expr | i <- B, !(i in A), qualifiers...]; int(1..2)])
 #[register_rule(("Base", 8700))]
 fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
     match expr {
-        Expr::Comprehension(_, comp) => {
+        Expr::AbstractComprehension(_, comp) => {
             // find if any of the generators are generating from expressions
-            for (name, decl) in comp.generator_submodel.symbols().clone().into_iter_local() {
-                if let DeclarationKind::ElementOf(gen_expr) = decl.kind() {
+            for qualifier in &comp.qualifiers {
+                if let Qualifier::Generator(Generator::ExpressionGenerator(ExpressionGenerator {
+                    decl,
+                    expression: gen_expr,
+                })) = qualifier
+                {
                     // match on expression being of form A union B
-                    match gen_expr {
-                        Expr::Union(_, a, b) => {
-                            // original comprehension parts common to both new
-                            let return_expr = comp.return_expression();
-                            let guards = comp.generator_submodel.constraints();
-                            let induction_variables = comp.induction_variables;
+                    if let Expr::Union(_, a, b) = gen_expr {
+                        // [ return_expr | i <- A, guards...] part
+                        let mut comprehension1 = comp.clone();
+                        // identify the generator qualifier again and modify the expression in place to be A
+                        for qualifier in &mut comprehension1.qualifiers {
+                            if let Qualifier::Generator(Generator::ExpressionGenerator(
+                                ExpressionGenerator {
+                                    decl: _,
+                                    expression,
+                                },
+                            )) = qualifier
+                            {
+                                *expression = a.clone().into();
+                            }
+                        }
 
-                            // create [ return_expr | i <- A, guards...] part
-                            let mut comprehension1 = ComprehensionBuilder::new(Rc::new(
-                                RefCell::new(comp.generator_submodel.symbols().clone()),
-                            ));
-                            let i1 = comprehension1
-                                .generator_symboltable()
-                                .borrow_mut()
-                                .gensym(&a);
-                            comprehension1 = comprehension1.generator(i1);
-                            comprehension1.guards = guards;
-                            comprehension1.induction_variables = induction_variables;
-
-                            // create [ return_expr | i <- B, !(i in A), guards...] part
-                            let mut comprehension2 = ComprehensionBuilder::new(Rc::new(
-                                RefCell::new(comp.generator_submodel.symbols().clone()),
-                            ));
-                            let i2 = comprehension2
-                                .generator_symboltable()
-                                .borrow_mut()
-                                .gensym(&b);
-                            comprehension2 = comprehension2.generator(i1);
-                            comprehension2 = comprehension2.guard(Expr::Not(
+                        // [ return_expr | i <- B, !(i in A), guards...] part
+                        let mut comprehension2 = comp.clone();
+                        // identify the generator qualifier again and modify the expression in place to be B
+                        for qualifier in &mut comprehension2.qualifiers {
+                            if let Qualifier::Generator(Generator::ExpressionGenerator(
+                                ExpressionGenerator {
+                                    decl: _,
+                                    expression,
+                                },
+                            )) = qualifier
+                            {
+                                *expression = b.clone().into();
+                            }
+                        }
+                        // add the condition !(i in A)
+                        comprehension2
+                            .qualifiers
+                            .push(Qualifier::Condition(Expr::Not(
                                 Metadata::new(),
                                 Moo::new(Expr::In(
                                     Metadata::new(),
-                                    Expr::Atomic(Metadata::new(), Atom::new_ref(i2)),
-                                    Moo::new(b.clone()),
-                                )),
-                            ));
-                            comprehension2.guards = guards;
-                            comprehension2.induction_variables = induction_variables;
-
-                            return Ok(Reduction::pure(Expr::Comprehension(
-                                Metadata::new(),
-                                Moo::new(Expr::Flatten(
-                                    Metadata::new(),
-                                    Moo::new(), // <-- don't know what do do about the fact flatten can take 1 or 2 arguments
-                                    Moo::new(
-                                        into_matrix_expr!(vec![comprehension1.with_return_value(return_expr, expr.to_ac_operator_kind()), comprehension2.with_return_value(return_expr, expr.to_ac_operator_kind())]; domain_int!(1..2)),
-                                    ),
+                                    Moo::new(Expr::Atomic(
+                                        Metadata::new(),
+                                        Atom::new_ref(decl.clone()),
+                                    )),
+                                    a.clone(),
                                 )),
                             )));
-                        }
-                        _ => Err(RuleNotApplicable),
+
+                        return Ok(Reduction::pure(Expr::Flatten(
+                            Metadata::new(),
+                            None,
+                            Moo::new(
+                                into_matrix_expr!(vec![Expr::AbstractComprehension(Metadata::new(), comprehension1), Expr::AbstractComprehension(Metadata::new(), comprehension2)]),
+                            ),
+                        )));
                     }
                 }
             }
