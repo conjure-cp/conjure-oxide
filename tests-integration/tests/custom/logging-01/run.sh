4c92acdf62c3da3011d739859b8809b3e6c07fc3
Shikhar Srivastava <shikhars2006@gmail.com>
2026-02-13 20:26:44 +0000

    corrected tests

--- tests-integration/tests/custom/logging-01/run.sh
+++ tests-integration/tests/custom/logging-01/run.sh
@@ -1,2 +1,7 @@
 conjure-oxide --logfile solve model.eprime
-cat ./conjure_oxide_log.json ./conjure_oxide.log
+
+[ -f "./conjure_oxide.log" ] && echo "./conjure_oxide.log found" || echo "./conjure_oxide.log is missing"
+[ -f "./conjure_oxide_log.json" ] && echo "./conjure_oxide_log.json found" || echo "./conjure_oxide_log.json is missing"
+
+[ -s "./conjure_oxide.log" ] && echo "./conjure_oxide.log has been written" || echo "./conjure_oxide.log is empty"
+[ -s "./conjure_oxide_log.json" ] && echo "./conjure_oxide_log.json has been written" || echo "./conjure_oxide_log.json is empty"
