8b6f7a8d39a4e92f4584d7886b2c231a8aec206f
Shikhar Srivastava <shikhars2006@gmail.com>
2026-02-13 16:32:03 +0000

    added tests for logging default and with logging turned on

---
+++ tests-integration/tests/custom/logging-01/run.sh
@@ -0,0 +1,2 @@
+conjure-oxide --logfile solve model.eprime
+cat ./conjure_oxide_log.json ./conjure_oxide.log
---
+++ tests-integration/tests/custom/no-logging/run.sh
@@ -0,0 +1,2 @@
+conjure-oxide solve model.eprime
+cat ./conjure_oxide_log.json ./conjure_oxide.log
