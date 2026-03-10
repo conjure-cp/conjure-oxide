a59a0382196c16c43625f98814b3646b2f561be1
Yehor Boiar <yb33@st-andrews.ac.uk>
2026-03-10 16:10:52 +0000

    docs: add todo with explanation

--- crates/conjure-cp-core/src/ast/eval.rs
+++ crates/conjure-cp-core/src/ast/eval.rs
@@ -565,7 +565,16 @@ pub fn eval_constant(expr: &Expr) -> Option<Lit> {
                 _ => None,
             }
         }
-        Expr::SATInt(..) => None,
+        Expr::SATInt(_, _, _, _) => {
+            // TODO: If this SATInt is composed of literals, we should evaluate it back to an
+            // integer literal.
+            //
+            // This is important because `is_all_constant` currently returns true for SATInts
+            // containing no references. If we don't evaluate them here, bubble rules will skip
+            // them (thinking they'll be constant-folded later), but they'll actually reach
+            // the solver adaptors as un-encoded unsafe operations, causing panics.
+            None
+        }
         Expr::PairwiseSum(_, a, b) => {
             match (eval_constant(a.as_ref())?, eval_constant(b.as_ref())?) {
                 (Lit::Int(a_int), Lit::Int(b_int)) => Some(Lit::Int(a_int + b_int)),
