6fc336709364cca4bd912978db4b89bd01b25d4f
leiamcalisteryoung <l.mcalisteryoung@gmail.com>
2026-03-12 17:22:33 +0000

    add decl_span map to parser, use it when vars are declared and referenced, update error messages

--- crates/conjure-cp-essence-parser/src/parser/parse_model.rs
+++ crates/conjure-cp-essence-parser/src/parser/parse_model.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use std::sync::{Arc, RwLock};
 use std::{fs, vec};
 
@@ -82,6 +83,7 @@ pub fn parse_essence_with_context_and_map(
 
     let mut model = Model::new(context);
     let mut source_map = SourceMap::default();
+    let mut declaration_spans = BTreeMap::new();
     let root_node = tree.root_node();
 
     // Create a ParseContext
@@ -91,6 +93,7 @@ pub fn parse_essence_with_context_and_map(
         Some(model.symbols_ptr_unchecked().clone()),
         errors,
         &mut source_map,
+        &mut declaration_spans,
     );
 
     let mut cursor = root_node.walk();
