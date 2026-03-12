ee79fde873ff69bcd2f51e1392a5ed0f05cd1458
Özgür Akgün <ozgurakgun@gmail.com>
2026-03-12 12:49:04 +0000

    make the shell script behave the same on macos and ubuntu

--- tests-integration/tests/custom/rule-trace-verbose/01/run.sh
+++ tests-integration/tests/custom/rule-trace-verbose/01/run.sh
@@ -1,9 +1,9 @@
 conjure-oxide solve pythagorean05.essence --rewriter naive --comprehension-expander via-solver-ac --rule-trace-verbose trace.csv --no-run-solver
-cat trace.csv | grep success | wc -l
-cat trace.csv | wc -l
+grep -c success trace.csv
+wc -l trace.csv | awk '{print $1}'
 rm trace.csv
 
 conjure-oxide solve pythagorean10.essence --rewriter naive --comprehension-expander via-solver-ac --rule-trace-verbose trace.csv --no-run-solver
-cat trace.csv | grep success | wc -l
-cat trace.csv | wc -l
+grep -c success trace.csv
+wc -l trace.csv | awk '{print $1}'
 rm trace.csv
