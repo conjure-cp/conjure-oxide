26cb3c7c9c4d88d88e60a65737da26bc366cebb3
Yi Xin Chong <yixinchong3010@gmail.com>
2026-03-15 19:25:53 +0000

    fix: hygiene

--- fuzz/fuzz_targets/error_detector.rs
+++ fuzz/fuzz_targets/error_detector.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
 use conjure_cp_essence_parser::detect_errors;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
