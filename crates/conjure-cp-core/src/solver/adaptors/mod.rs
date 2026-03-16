0fa8605dde29470483919c0363f242eb7040d418
AlexFaquet <alexfaquet2003@gmail.com>
2026-03-16 22:31:41 +0000

    feat(solver): add SavileRow solver adaptor stub

--- crates/conjure-cp-core/src/solver/adaptors/mod.rs
+++ crates/conjure-cp-core/src/solver/adaptors/mod.rs
@@ -2,6 +2,7 @@
 
 pub mod minion;
 pub mod rustsat;
+pub mod savile_row;
 
 #[doc(inline)]
 pub use minion::Minion;
@@ -9,6 +10,8 @@ pub use minion::Minion;
 #[doc(inline)]
 pub use rustsat::Sat;
 
+#[doc(inline)]
+pub use savile_row::SavileRow
 pub mod smt;
 
 #[doc(inline)]
---
+++ crates/conjure-cp-core/src/solver/adaptors/savile_row/adaptors.rs
@@ -0,0 +1,80 @@
+//! Solver adaptor for Savile Row, called as an external process.
+
+use std::io::Write;
+
+use crate::Model;
+use crate::settings::SolverFamily;
+use crate::solver::{
+    SearchComplete, SearchStatus, SolveSuccess, SolverAdaptor, SolverCallback, SolverError,
+    SolverMutCallback, private,
+};
+use crate::stats::SolverStats;
+
+/// Solver adaptor for Savile Row.
+///
+/// Savile Row is called as an external process. The model is written to a
+/// temporary file, Savile Row is invoked, and its output is parsed back
+/// into Conjure Oxide's solution format.
+pub struct SavileRow {
+    model: Option<Model>,
+}
+
+impl SavileRow {
+    pub fn new() -> Self {
+        SavileRow { model: None }
+    }
+}
+
+impl Default for SavileRow {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl private::Sealed for SavileRow {}
+
+impl SolverAdaptor for SavileRow {
+    fn load_model(
+        &mut self,
+        model: Model,
+        _: private::Internal,
+    ) -> Result<(), SolverError> {
+        self.model = Some(model);
+        Ok(())
+    }
+
+    fn solve(
+        &mut self,
+        _callback: SolverCallback,
+        _: private::Internal,
+    ) -> Result<SolveSuccess, SolverError> {
+        Err(SolverError::OpNotImplemented(
+            "Savile Row solve not yet implemented".into(),
+        ))
+    }
+
+    fn solve_mut(
+        &mut self,
+        _callback: SolverMutCallback,
+        _: private::Internal,
+    ) -> Result<SolveSuccess, SolverError> {
+        Err(SolverError::OpNotSupported(
+            "Savile Row does not support incremental solving".into(),
+        ))
+    }
+
+    fn get_family(&self) -> SolverFamily {
+        SolverFamily::SavileRow
+    }
+
+    fn get_name(&self) -> &'static str {
+        "SavileRow"
+    }
+
+    fn write_solver_input_file(
+        &self,
+        writer: &mut Box<dyn Write>,
+    ) -> Result<(), std::io::Error> {
+        writer.write_all(b"// Savile Row input file (not yet implemented)\n")
+    }
+}
