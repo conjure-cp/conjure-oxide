9c63e089baa23d089ded5dccd1d66415f550b176
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2026-03-25 16:54:44 +0000

    Separate out getting union operands because lock stuf

--- crates/conjure-cp-rules/src/sets/horizontal/union.rs
+++ crates/conjure-cp-rules/src/sets/horizontal/union.rs
@@ -18,58 +18,64 @@ fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
                     let decl = ptr.clone();
 
                     // match on expression being of form A union B
-                    if let Some(expr) = ptr.as_quantified_expr() {
-                        if let Expr::Union(_, a, b) = &*expr {
-                            let a = a.clone();
-                            let b = b.clone();
+                    let Some((a, b)) = (match ptr.as_quantified_expr() {
+                        Some(expr_guard) => match &*expr_guard {
+                            Expr::Union(_, a, b) => Some((a.clone(), b.clone())),
+                            _ => None,
+                        },
+                        None => None,
+                    }) else {
+                        continue;
+                    };
 
-                            // [ return_expr | i <- A, guards...] part
-                            let mut comprehension1 = comp.clone();
-                            // modify the generator expression in place to be A
-                            if let Some(qual) = comprehension1.qualifiers.get_mut(i) {
-                                if let ComprehensionQualifier::ExpressionGenerator { ptr } = qual {
-                                    if let Some(mut expr) = ptr.as_quantified_expr_mut() {
-                                        *expr = a.clone().into();
-                                    }
-                                }
-                            }
-
-                            // [ return_expr | i <- B, !(i in A), guards...] part
-                            let mut comprehension2 = comp.clone();
-                            // identify the generator qualifier again and modify the expression in place to be B
-                            if let Some(qual) = comprehension2.qualifiers.get_mut(i) {
-                                if let ComprehensionQualifier::ExpressionGenerator { ptr } = qual {
-                                    if let Some(mut expr) = ptr.as_quantified_expr_mut() {
-                                        *expr = b.into();
-                                    }
-                                }
-                            }
-
-                            // add the condition !(i in A)
-                            comprehension2
-                                .qualifiers
-                                .push(ComprehensionQualifier::Condition(Expr::Not(
-                                    Metadata::new(),
-                                    Moo::new(Expr::In(
-                                        Metadata::new(),
-                                        Moo::new(Expr::Atomic(
-                                            Metadata::new(),
-                                            Atom::new_ref(decl),
-                                        )),
-                                        a.clone(),
-                                    )),
-                                )));
-
-                            return Ok(Reduction::pure(Expr::Flatten(
-                                Metadata::new(),
-                                None,
-                                Moo::new(into_matrix_expr!(vec![
-                                    Expr::Comprehension(Metadata::new(), comprehension1),
-                                    Expr::Comprehension(Metadata::new(), comprehension2)
-                                ])),
-                            )));
-                        }
+                    // [ return_expr | i <- A, guards...] part
+                    let mut comprehension1 = comp.clone();
+                    // modify the generator expression in place to be A
+                    if let Some(qual) = comprehension1.qualifiers.get_mut(i)
+                        && let ComprehensionQualifier::ExpressionGenerator { ptr } = qual
+                        && let Some(mut expr) = ptr.as_quantified_expr_mut()
+                    {
+                        *expr = a.clone().into();
+                    } else {
+                        panic!(
+                            "union_set rule could not find ExpressionGenerator expr while trying to modify first comp in place to a"
+                        );
                     }
+
+                    // [ return_expr | i <- B, !(i in A), guards...] part
+                    let mut comprehension2 = comp.clone();
+                    // identify the generator qualifier again and modify the expression in place to be B
+                    if let Some(qual) = comprehension2.qualifiers.get_mut(i)
+                        && let ComprehensionQualifier::ExpressionGenerator { ptr } = qual
+                        && let Some(mut expr) = ptr.as_quantified_expr_mut()
+                    {
+                        *expr = b.into();
+                    } else {
+                        panic!(
+                            "union_set rule could not find ExpressionGenerator expr in comprehension2 while trying to change to B in place"
+                        );
+                    }
+
+                    // add the condition !(i in A)
+                    comprehension2
+                        .qualifiers
+                        .push(ComprehensionQualifier::Condition(Expr::Not(
+                            Metadata::new(),
+                            Moo::new(Expr::In(
+                                Metadata::new(),
+                                Moo::new(Expr::Atomic(Metadata::new(), Atom::new_ref(decl))),
+                                a.clone(),
+                            )),
+                        )));
+
+                    return Ok(Reduction::pure(Expr::Flatten(
+                        Metadata::new(),
+                        None,
+                        Moo::new(into_matrix_expr!(vec![
+                            Expr::Comprehension(Metadata::new(), comprehension1),
+                            Expr::Comprehension(Metadata::new(), comprehension2)
+                        ])),
+                    )));
                 }
             }
 
