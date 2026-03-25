32fdfc30cc8f9d702dd82a1c94924795884885e3
Georgii Skorokhod <skorokhod.g@gmail.com>
2026-03-25 11:28:52 +0000

    WIP

--- crates/conjure-cp-rules/src/representation/mod.rs
+++ crates/conjure-cp-rules/src/representation/mod.rs
@@ -18,7 +18,7 @@ mod prelude {
 pub mod matrix_to_atom;
 pub mod record_to_atom;
 pub mod sat_direct_int;
-mod set_explicit;
+pub mod set_explicit;
 pub mod tuple_to_atom;
 // mod sat_log_int;
 // mod sat_order_int;
