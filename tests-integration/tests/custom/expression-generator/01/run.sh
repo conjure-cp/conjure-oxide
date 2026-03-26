1c6ffd616400ec0af1fc769082669d54dcdc06fb
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2026-03-26 00:35:06 +0000

    Add solve to custom test to show final essence model and rule trace

--- tests-integration/tests/custom/expression-generator/01/run.sh
+++ tests-integration/tests/custom/expression-generator/01/run.sh
@@ -1,1 +1,5 @@
 conjure-oxide pretty model.essence --output-format="ast-json"
+conjure-oxide-debug solve model.essence --no-run-solver --rule-trace-verbose trace.csv
+grep -c success trace.csv
+wc -l trace.csv | awk '{print $1}'
+rm trace.csv
