32181cf69b73dd6ae9fdbfae85ecc6c219394ec2
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2026-03-26 20:56:19 +0000

    Replace references in comprehension2 with ref to new generator

--- crates/conjure-cp-rules/src/sets/horizontal/union.rs
+++ crates/conjure-cp-rules/src/sets/horizontal/union.rs
@@ -1,11 +1,12 @@
 use conjure_cp::ast::comprehension::ComprehensionQualifier;
-use conjure_cp::ast::{Atom, Metadata};
+use conjure_cp::ast::{Atom, DeclarationPtr, Metadata};
 use conjure_cp::ast::{Expression as Expr, Moo, SymbolTable};
 use conjure_cp::into_matrix_expr;
 use conjure_cp::rule_engine::Reduction;
 use conjure_cp::rule_engine::{
     ApplicationError::RuleNotApplicable, ApplicationResult, register_rule,
 };
+use uniplate::Biplate;
 
 // [ return_expr | i <- A union B, qualifiers...] -> flatten([[ return_expr | i <- A, qualifiers...], [ return_expr | i <- B, !(i in A), qualifiers...]; int(1..2)])
 #[register_rule(("Base", 8700))]
@@ -15,7 +16,7 @@ fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
             // find if any of the generators are generating from expressions
             for (i, qualifier) in comp.qualifiers.iter().enumerate() {
                 if let ComprehensionQualifier::ExpressionGenerator { ptr } = qualifier {
-                    let decl = ptr.clone();
+                    let gen_decl = ptr.clone();
 
                     // match on expression being of form A union B
                     let Some((a, b)) = (match ptr.as_quantified_expr() {
@@ -44,17 +45,24 @@ fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
 
                     // [ return_expr | i <- B, !(i in A), guards...] part
                     let mut comprehension2 = comp.clone();
-                    // identify the generator qualifier again and modify the expression in place to be B
-                    if let Some(qual) = comprehension2.qualifiers.get_mut(i)
-                        && let ComprehensionQualifier::ExpressionGenerator { ptr } = qual
-                        && let Some(mut expr) = ptr.as_quantified_expr_mut()
-                    {
-                        *expr = b.into();
-                    } else {
-                        panic!(
-                            "union_set rule could not find ExpressionGenerator expr in comprehension2 while trying to change to B in place"
-                        );
-                    }
+                    // modify the generator to be just from b
+                    let b_ptr =  DeclarationPtr::new_quantified_expr(gen_decl.name().clone(), b.into());
+                    let b_gen = ComprehensionQualifier::ExpressionGenerator { ptr: b_ptr.clone() };
+                    if let Some(qual2) = comprehension2.qualifiers.get_mut(i) {
+                        *qual2 = b_gen;
+                    } 
+
+                    // replace all occurences of old generator in comprehension2 with new pointer
+                    comprehension2.return_expression.transform_bi(&|atom: Atom| match atom {
+                        Atom::Reference(reference) => {
+                            if reference.clone().into_ptr() == gen_decl {
+                                Atom::new_ref(b_ptr.clone())
+                            } else {
+                                Atom::Reference(reference)
+                            }
+                        }
+                        other => other,
+                    });
 
                     // add the condition !(i in A)
                     comprehension2
@@ -63,7 +71,7 @@ fn union_set(expr: &Expr, _: &SymbolTable) -> ApplicationResult {
                             Metadata::new(),
                             Moo::new(Expr::In(
                                 Metadata::new(),
-                                Moo::new(Expr::Atomic(Metadata::new(), Atom::new_ref(decl))),
+                                Moo::new(Expr::Atomic(Metadata::new(), Atom::new_ref(b_ptr))),
                                 a.clone(),
                             )),
                         )));
