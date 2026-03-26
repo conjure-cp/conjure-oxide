32ed46d116867a689c4d4787447db68d83c6eab9
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2026-03-26 17:30:51 +0000

    Add human readable rule trace

--- tests-integration/tests/custom/expression-generator/01/run.sh
+++ tests-integration/tests/custom/expression-generator/01/run.sh
@@ -1,5 +1,9 @@
 conjure-oxide pretty model.essence --output-format="ast-json"
-conjure-oxide-debug solve model.essence --no-run-solver --rule-trace-verbose trace.csv
-grep -c success trace.csv
-wc -l trace.csv | awk '{print $1}'
+conjure-oxide-debug solve model.essence --no-run-solver --rule-trace-verbose trace-verbose.csv --rule-trace trace.csv
+
+grep -c success trace-verbose.csv
+wc -l trace-verbose.csv | awk '{print $1}'
+rm trace-verbose.csv
+
+cat trace.csv
 rm trace.csv
