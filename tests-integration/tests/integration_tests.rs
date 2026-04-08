0da4528bbe35088389ca1c08c2c0e9781cd50d23
Özgür Akgün <ozgurakgun@gmail.com>
2026-04-08 10:47:36 +0100

    feat(morph): enable morph for as many integration tests as possible, fix issues for the sat backend (#1754)
    to be written...

--- tests-integration/tests/integration_tests.rs
+++ tests-integration/tests/integration_tests.rs
@@ -146,8 +146,8 @@ fn integration_test(path: &str, essence_base: &str, extension: &str) -> Result<(
                         as Arc<dyn tracing::Subscriber + Send + Sync>;
                     let run_label = run_case_label(path, essence_base, extension, run_case);
                     eprintln!("[integration] running {run_label}");
-                    let default_rule_trace_enabled = true;
-                    set_rule_trace_enabled(default_rule_trace_enabled);
+                    let default_rule_trace_enabled = matches!(rewriter, Rewriter::Naive);
+                    set_rule_trace_enabled(true);
                     set_default_rule_trace_enabled(default_rule_trace_enabled);
                     set_rule_trace_verbose_enabled(false);
                     set_rule_trace_aggregates_enabled(false);
