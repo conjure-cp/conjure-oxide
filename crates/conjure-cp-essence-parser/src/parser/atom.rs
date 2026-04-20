dd1332146c235db09ba15754d23d117f284a8192
Calum C <cc398@st-andrews.ac.uk>
2026-04-20 16:16:35 +0100

    bringing up to date w main

--- crates/conjure-cp-essence-parser/src/parser/atom.rs
+++ crates/conjure-cp-essence-parser/src/parser/atom.rs
@@ -349,6 +349,7 @@ fn typecheck_variable(
         TypecheckingContext::Matrix => "matrix",
         TypecheckingContext::Tuple => "tuple",
         TypecheckingContext::Record => "record",
+        TypecheckingContext::Sequence => "sequence",
         TypecheckingContext::Unknown => return None, // shouldn't reach here
     };
 
@@ -436,6 +437,7 @@ fn parse_constant(ctx: &mut ParseContext, node: &Node) -> Result<Option<Literal>
             TypecheckingContext::Matrix => "matrix",
             TypecheckingContext::Tuple => "tuple",
             TypecheckingContext::Record => "record",
+            TypecheckingContext::Sequence => "sequence",
             TypecheckingContext::Unknown => "",
         };
 
