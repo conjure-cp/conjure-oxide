f4b1715804fa8f3c9634819d2fbe7d98220365af
Nicholas Davidson <nd65@st-andrews.ac.uk>
2026-04-21 17:41:12 +0100

    fix: updated to work with main

--- crates/conjure-cp-essence-parser/src/parser/expression.rs
+++ crates/conjure-cp-essence-parser/src/parser/expression.rs
@@ -623,6 +623,8 @@ fn inferred_context_from_expression(expr: &Expression) -> TypecheckingContext {
         GroundDomain::Matrix(_, _) => TypecheckingContext::Matrix,
         GroundDomain::Tuple(_) => TypecheckingContext::Tuple,
         GroundDomain::Record(_) => TypecheckingContext::Record,
-        GroundDomain::Function(_, _, _) | GroundDomain::Empty(_) => TypecheckingContext::Unknown,
+        GroundDomain::Function(_, _, _) | GroundDomain::Variant(_) | GroundDomain::Empty(_) => {
+            TypecheckingContext::Unknown
+        }
     }
 }
