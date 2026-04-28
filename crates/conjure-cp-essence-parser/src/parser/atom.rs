d2ffad1e327e68fedbaebca0cf7ee301b526a4ca
Nicholas Davidson <nd65@st-andrews.ac.uk>
2026-04-28 12:14:20 +0100

    fix: merge with main

--- crates/conjure-cp-essence-parser/src/parser/atom.rs
+++ crates/conjure-cp-essence-parser/src/parser/atom.rs
@@ -298,7 +298,7 @@ fn parse_variable(ctx: &mut ParseContext, node: &Node) -> Result<Option<Atom>, F
                 DeclarationKind::DomainLetting(_) => SymbolKind::LettingVar,
                 DeclarationKind::Quantified(..) => SymbolKind::FindVar,
                 DeclarationKind::QuantifiedExpr(..) => SymbolKind::FindVar,
-                DeclarationKind::RecordField(_) => SymbolKind::Decimal,
+                DeclarationKind::Field(_) => SymbolKind::Decimal,
                 &_ => todo!(),
             };
 
