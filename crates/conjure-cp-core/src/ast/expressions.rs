6df25c6f5d97693d12b013bd5792c49d5e449aea
Calum C <cc398@st-andrews.ac.uk>
2026-04-28 14:48:29 +0100

    fmt

--- crates/conjure-cp-core/src/ast/expressions.rs
+++ crates/conjure-cp-core/src/ast/expressions.rs
@@ -1385,7 +1385,10 @@ impl Expression {
                     _ => {}
                 }
 
-                Some(Domain::set(SetAttr::new_size(len), Domain::int(inner.as_int()?)))
+                Some(Domain::set(
+                    SetAttr::new_size(len),
+                    Domain::int(inner.as_int()?),
+                ))
             }
             Expression::Party(_, _, p) => {
                 // Will pick a part, so set will share same attrs
