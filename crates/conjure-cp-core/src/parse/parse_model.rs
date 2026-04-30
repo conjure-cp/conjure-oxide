a91e86d789feef6fbe96db7f3868339215a07d95
Nicholas Davidson <nd65@st-andrews.ac.uk>
2026-04-30 17:03:55 +0100

    fmt

--- crates/conjure-cp-core/src/parse/parse_model.rs
+++ crates/conjure-cp-core/src/parse/parse_model.rs
@@ -14,8 +14,8 @@ use crate::ast::comprehension::ComprehensionBuilder;
 use crate::ast::records::FieldValue;
 use crate::ast::{
     AbstractLiteral, Atom, BinaryAttr, DeclarationPtr, Domain, Expression, FieldEntry, FuncAttr,
-    IntVal, JectivityAttr, Literal, MSetAttr, Name, PartialityAttr, Range, RelAttr, ReturnType, SequenceAttr,
-    SetAttr, SymbolTable, SymbolTablePtr,
+    IntVal, JectivityAttr, Literal, MSetAttr, Name, PartialityAttr, Range, RelAttr, ReturnType,
+    SequenceAttr, SetAttr, SymbolTable, SymbolTablePtr,
 };
 use crate::ast::{DomainPtr, Metadata};
 use crate::context::Context;
