ab147e75a2dcccb288bdd6223b0dbb1a23a30cf6
Özgür Akgün <ozgurakgun@gmail.com>
2026-06-22 16:08:35 +0100

    test(test-suite): set parser=via-conjure now that the default has changed

--- test-suite/tests/custom/expression-generator/02/union1/run.sh
+++ test-suite/tests/custom/expression-generator/02/union1/run.sh
@@ -1,9 +1,9 @@
-conjure-oxide pretty model.essence --output-format="ast-json"
+conjure-oxide --parser=via-conjure pretty model.essence --output-format="ast-json"
 
 echo ""
 echo ""
 echo ""
 
-conjure-oxide-debug solve model.essence --no-run-solver --rule-trace trace.txt
+conjure-oxide-debug --parser=via-conjure solve model.essence --no-run-solver --rule-trace trace.txt
 cat trace.txt
 rm trace.txt
