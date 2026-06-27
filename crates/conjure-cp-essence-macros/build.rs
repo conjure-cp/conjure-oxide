eab69080b1c25cea2903519e945640229c771010
CodingCellist <teh6@st-andrews.ac.uk>
2026-06-27 15:06:17 +0100

    build(deps): Search common paths for libz3 (#1882)
    (Sorry, I did have a read through `CONTRIBUTING.md`, but didn't seem to
    have permission to create a branch, hence a fork+PR.)
    
    ## Description
    
    When trying to build 734f4f65df42179e1353489c1b0ce1b5612b2d50 on my
    local MacBook (Apple Silicon M5, macOS Tahoe 26.5.1, zsh 5.9, omz
    5ef67be, rustup 1.29.0 (2026-03-05), rustc 1.96.0 (ac68faa20
    2026-05-25), cargo 1.96.0 (30a34c682 2026-05-25)) both `make install`
    and `cargo build` failed with the error message:
    
    ```
    Compiling conjure-cp v0.1.0 (<path>)
    error[E0463]: can't find crate for `conjure_cp_essence_macros`
      --> crates/conjure-cp/src/lib.rs:2:9
       |
     2 | pub use conjure_cp_essence_macros::*;
       |         ^^^^^^^^^^^^^^^^^^^^^^^^^ can't find crate
    
    For more information about this error, try `rustc --explain E0463`.
    error: could not compile `conjure-cp` (lib) due to 1 previous error
    `conjure-cp-essence-macros
    ```
    
    This turned out to be due to `libz3.dylib` not being found in my
    `DYLD_LIBRARY_PATH`, despite having both a full install of Z3 via
    `brew`, as well as a copy of `libz3.dylid` from the Conjure 2.6.0 "with
    solvers" install for non-Intel macOS
    (https://github.com/conjure-cp/conjure/releases/tag/v2.6.0).
    
    Manually exporting `DYLD_LIBRARY_PATH` to any of the directories where
    my copies of `libz3.dylib` were (`/opt/homebrew/lib/`,
    `/opt/homebrew/Cellar/z3/4.16.0/lib/`, and
    `${HOME}/.local/bin/conjure-v2.6.0-macos-with-solvers/lib/`) raised
    several permissions error dialogs on macOS. In the end, I found [the
    Rust build script
    docs](https://doc.rust-lang.org/cargo/reference/build-scripts.html)
    which helped me put together the `build.rs` in this commit.
    
    While this did fix things for me, it is entirely possible that this is
    not the best solution; I am not a seasoned Rust developer. However, as
    far as I understand, the added `build.rs` should provide some sane
    defaults, with people being able to symlink a local version (or put it
    in a project-local `lib` directory) if they need that. All the script
    does is enumerate the usual paths and library names, and then emit some
    instructions to `cargo` to include those paths when searching for the Z3
    library.
    
    That said, both Oz (ozgurakgun) and Ferdia (fmckeogh) were able to
    compile the same commit on their Apple Silicon MacBooks with the same
    Rust toolchain as me, so it could very well be that I missed a setup
    step for dynamic libraries or similar.
    
    ## Related issues
    
    None.
    
    ## Key changes
    
    * Adds a `build.rs` file which enumerates standard locations for the Z3
    solver's dynamic library to `crates/conjure-cp-essence-macros/`.
    
    ## How to test/review
    
    * Set up a fresh dev environment with the usual environment variables,
    but with an empty (default?) `DYLD_LIBRARY_PATH`.
    * Try to build the latest main, this should fail as in the description
    above due to libz3 not being in a standard location.
    * Building with this PR should succeed.
    
    ---------
    
    Co-authored-by: Özgür Akgün <ozgurakgun@gmail.com>

---
+++ crates/conjure-cp-essence-macros/build.rs
@@ -0,0 +1,39 @@
+use std::path::PathBuf;
+
+fn main() {
+    // paths to search for Z3 dylib
+    let candidates: Vec<PathBuf> = [
+        // macOS
+        "/opt/homebrew/lib",
+        "/usr/local/lib",
+        // linux
+        "/usr/lib",
+        "/usr/lib64",
+        "/lib",
+        "/lib64",
+    ]
+    .into_iter()
+    .map(PathBuf::from)
+    .collect();
+
+    let mut valid_paths = Vec::new();
+
+    // check possible lib names
+    for dir in candidates {
+        if dir.join("libz3.dylib").exists()
+            || dir.join("libz3.so").exists()
+            || dir.join("libz3.a").exists()
+        {
+            valid_paths.push(dir);
+        }
+    }
+    // if all else failed, maybe it is local?
+    valid_paths.push(PathBuf::from("@executable_path/../lib"));
+
+    // do some horrible linker things
+    for path in &valid_paths {
+        if let Some(p) = path.to_str() {
+            println!("cargo:rustc-link-search=native={}", p);
+        }
+    }
+}
